47fa74d3dabceaf9f81744e3f89fccf51d3bfa34
Simon Urli <simon.urli@inria.fr>
2017-02-28 16:33:34 +0100

    Improve scripts by passing repairnator home dir as a variable

--- scripts/build_repairnator.sh
+++ scripts/build_repairnator.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cd /home/repairnator/github/nopol/nopol
+export M2_HOME=/opt/apache-maven-3.3.9
+export PATH=$PATH:$M2_HOME/bin
+
+HOME_REPAIR=/media/experimentations/repairnator
+
+cd $HOME_REPAIR/github/nopol/nopol
 git pull
 mvn clean install
 
-cd /home/repairnator/github/librepair
+cd $HOME_REPAIR/github/librepair
 git pull
 
 cd jtravis
@@ -15,7 +20,7 @@ mvn clean install
 
 if [[ $? == 0 ]]
 then
-   cp -f ./target/repairnator-1.0-SNAPSHOT-jar-with-dependencies.jar /home/repairnator/scripts/repairnator.jar
+   cp -f ./target/repairnator-1.0-SNAPSHOT-jar-with-dependencies.jar $HOME_REPAIR/scripts/repairnator.jar
 else
    echo "Error while building a new version of repairnator"
 fi
--- scripts/launch_repairnator.sh
+++ scripts/launch_repairnator.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+HOME_REPAIR=/media/experimentations/repairnator
+
 RUNNING_REPAIR=`ps aux |grep "[r]epairnator.jar" |wc -l`
 if [[ $RUNNING_REPAIR == 0 ]]
 then
-   /home/repairnator/scripts/build_repairnator.sh
+   $HOME_REPAIR/scripts/build_repairnator.sh
 fi
 
 export M2_HOME=/opt/apache-maven-3.3.9
 export GITHUB_OAUTH=
-TOOLS_PATH=/usr/lib/jvm/java-8-oracle/lib/tools.jar
-REPAIR_JAR=/home/repairnator/scripts/repairnator.jar
+TOOLS_PATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar
+REPAIR_JAR=$HOME_REPAIR/scripts/repairnator.jar
 
-LOG_FILE=/home/repairnator/logs/output_`date "+%Y-%m-%d_%H%M"`.log
-NB_HOURS=4
-Z3_PATH=/home/repairnator/github/nopol/nopol/lib/z3/z3_for_linux
-WORKSPACE_PATH=/home/repairnator/workspace/
-OUTPUT_PATH=/var/www/html/repair/
-PROJECT_LIST_PATH=/home/repairnator/scripts/project_list.txt
+LOG_FILE=$HOME_REPAIR/logs/output_`date "+%Y-%m-%d_%H%M"`.log
+NB_HOURS=6
+Z3_PATH=$HOME_REPAIR/github/nopol/nopol/lib/z3/z3_for_linux
+WORKSPACE_PATH=$HOME_REPAIR/workspace/
+OUTPUT_PATH=/var/www/html/repairnator/
+PROJECT_LIST_PATH=$HOME_REPAIR/scripts/project_list.txt
 
 REPAIR_ARGS="-m slug -i $PROJECT_LIST_PATH -o $OUTPUT_PATH -w $WORKSPACE_PATH -z $Z3_PATH -p -l $NB_HOURS --clean"
 
