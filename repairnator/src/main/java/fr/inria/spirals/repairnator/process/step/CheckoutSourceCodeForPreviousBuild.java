a9746b695a83361eb99583381c53fa4550530164
fermadeiral <fer.madeiral@gmail.com>
2017-03-01 17:43:24 +0100

    Moving methods from CloneRepository class into the new GitHelper class. (#63)
    * Moving methods from CloneRepository class into the new GitHelper class
    
    * Some changes at moving methods from CloneRepository class to the new GitHelper class and extracting mergeTwoCommitsForPR method from CloneRepository to GitHelper
    
    * Some changes at moving methods from CloneRepository class to the new GitHelper class and extracting mergeTwoCommitsForPR method from CloneRepository to GitHelper
    
    * Testing commit existence before checkout in CheckoutSourceCodeForPreviousBuild class and logging problem when the merging of two commits to reproduce the PR doesnt succeed
    
    Fix #64

--- repairnator/src/main/java/fr/inria/spirals/repairnator/process/step/CheckoutSourceCodeForPreviousBuild.java
+++ repairnator/src/main/java/fr/inria/spirals/repairnator/process/step/CheckoutSourceCodeForPreviousBuild.java
@@ -1,6 +1,8 @@
 package fr.inria.spirals.repairnator.process.step;
 
+import fr.inria.spirals.jtravis.entities.Build;
 import fr.inria.spirals.repairnator.process.ProjectState;
+import fr.inria.spirals.repairnator.process.git.GitHelper;
 import fr.inria.spirals.repairnator.process.inspectors.ProjectInspector;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -23,23 +25,35 @@ public class CheckoutSourceCodeForPreviousBuild extends CloneRepository {
         Git git;
         try {
             git = Git.open(new File(inspector.getRepoLocalPath()));
-            String commitCheckout = inspector.getBuild().getCommit().getSha();
-            commitCheckout = this.testCommitExistence(git, commitCheckout);
+            Build build = inspector.getBuild();
+            String commitCheckout = build.getCommit().getSha();
+            commitCheckout = GitHelper.testCommitExistence(git, commitCheckout, this, build);
             if (commitCheckout != null) {
-                this.getLogger()
-                        .debug("Get the commit " + commitCheckout + " for repo " + this.inspector.getRepoSlug());
+                this.getLogger().debug("Get the commit " + commitCheckout + " for repo " + this.inspector.getRepoSlug());
                 git.checkout().setName(commitCheckout).call();
-                git.checkout().setStartPoint(inspector.getPreviousBuild().getCommit().getSha()).addPath("src/main/java")
-                        .call();
-                this.state = ProjectState.PREVIOUSBUILDCODECHECKEDOUT;
+                Build previousBuild = inspector.getPreviousBuild();
+                commitCheckout = previousBuild.getCommit().getSha();
+                commitCheckout = GitHelper.testCommitExistence(git, commitCheckout, this, previousBuild);
+                if (commitCheckout != null) {
+                    this.getLogger().debug("Get the commit " + commitCheckout + " for repo " + this.inspector.getRepoSlug());
+                    git.checkout().setStartPoint(inspector.getPreviousBuild().getCommit().getSha()).addPath("src/main/java").call();
+                    this.state = ProjectState.PREVIOUSBUILDCODECHECKEDOUT;
+                } else {
+                    this.addStepError("Error while getting the commit " + commitCheckout + " to checkout from the repo.");
+                    this.shouldStop = true;
+                    this.state = ProjectState.PREVIOUSBUILDCODENOTCHECKEDOUT;
+                    return;
+                }
             } else {
-                this.addStepError("Error while getting the commit to checkout from the repo.");
+                this.addStepError("Error while getting the commit " + commitCheckout + " to checkout from the repo.");
                 this.shouldStop = true;
                 this.state = ProjectState.PREVIOUSBUILDCODENOTCHECKEDOUT;
                 return;
             }
         } catch (IOException | GitAPIException e) {
-            e.printStackTrace();
+            this.addStepError("Error while getting the commit to checkout from the repo.");
+            this.shouldStop = true;
+            this.state = ProjectState.PREVIOUSBUILDCODENOTCHECKEDOUT;
         }
     }
 
