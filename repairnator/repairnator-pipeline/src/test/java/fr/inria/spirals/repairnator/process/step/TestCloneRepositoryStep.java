c15515742a73bef5c7215df316cea833539ec243
Simon Urli <simon.urli@inria.fr>
2017-03-06 15:15:20 +0100

    Add a new test end-to-end for clone step. Slight change in CloneRepository step.

--- repairnator/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/process/step/TestCloneRepositoryStep.java
+++ repairnator/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/process/step/TestCloneRepositoryStep.java
@@ -1,7 +1,80 @@
 package fr.inria.spirals.repairnator.process.step;
 
+import fr.inria.spirals.jtravis.entities.Build;
+import fr.inria.spirals.jtravis.helpers.BuildHelper;
+import fr.inria.spirals.repairnator.BuildToBeInspected;
+import fr.inria.spirals.repairnator.ProjectState;
+import fr.inria.spirals.repairnator.ScannedBuildStatus;
+import fr.inria.spirals.repairnator.process.inspectors.ProjectInspector;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by urli on 21/02/2017.
  */
 public class TestCloneRepositoryStep {
+
+    @Test
+    public void testCloneMasterBuild() throws IOException, GitAPIException {
+        int buildId = 207924136; // surli/failingProject build
+
+        Build build = BuildHelper.getBuildFromId(buildId, null);
+        assertThat(build, notNullValue());
+        assertThat(buildId, is(build.getId()));
+
+        Path tmpDirPath = Files.createTempDirectory("test_clone");
+        File tmpDir = tmpDirPath.toFile();
+        tmpDir.deleteOnExit();
+
+        BuildToBeInspected toBeInspected = new BuildToBeInspected(build, ScannedBuildStatus.ONLY_FAIL);
+
+        ProjectInspector inspector = mock(ProjectInspector.class);
+        when(inspector.getWorkspace()).thenReturn(tmpDir.getAbsolutePath());
+        when(inspector.getRepoLocalPath()).thenReturn(tmpDir.getAbsolutePath());
+        when(inspector.getBuildToBeInspected()).thenReturn(toBeInspected);
+        when(inspector.getBuild()).thenReturn(build);
+
+        CloneRepository cloneStep = new CloneRepository(inspector);
+        cloneStep.execute();
+
+        assertThat(cloneStep.getState(), is(ProjectState.CLONABLE));
+        assertThat(cloneStep.shouldStop, is(false));
+
+        Git gitDir = Git.open(tmpDir);
+        Ref ref = gitDir.getRepository().exactRef("HEAD");
+
+        assertThat(ref.isSymbolic(), is(true));
+
+        ref = ref.getTarget();
+
+        assertThat(ref.getObjectId().getName(), not(build.getCommit().getSha())); // no check out yet
+
+        Status status = gitDir.status().call();
+        assertThat(status.getUntracked().size(), is(1));
+        assertThat(status.getAdded().isEmpty(), is(true));
+        assertThat(status.getChanged().isEmpty(), is(true));
+        assertThat(status.getModified().isEmpty(), is(true));
+        assertThat(status.getRemoved().isEmpty(), is(true));
+
+        Set<String> addedFile = status.getUntracked();
+
+        String[] propertyFile = addedFile.toArray(new String[addedFile.size()]);
+
+        assertThat(propertyFile[0], is("repairnator.properties"));
+    }
 }
