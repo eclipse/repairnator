b3e7b0c29e5b5fdb7d207d0baa824534e6e5c1f4
fermadeiral <fer.madeiral@gmail.com>
2017-03-07 15:36:21 +0100

    Splitting LauncherMode into LauncherMode and FileMode (issue #90) (#101)
    * Splitting LauncherMode into LauncherMode and FileMode (issue #90)
    
    * Changing LauncherMode.REPAIRNATOR to LauncherMode.REPAIR and assigning the responsibility to get builds based on LauncherMode and FileMode to ProjectScanner
    
    * Updating the documentation (usage) of RepairNator (repairnator/Readme.md file)
    
    * Updating launch_repairnator.sh file
    
    Fix #90

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/inspectors/ProjectInspector.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/inspectors/ProjectInspector.java
@@ -2,7 +2,6 @@ package fr.inria.spirals.repairnator.process.inspectors;
 
 import fr.inria.spirals.jtravis.entities.Build;
 import fr.inria.spirals.repairnator.BuildToBeInspected;
-import fr.inria.spirals.repairnator.LauncherMode;
 import fr.inria.spirals.repairnator.ProjectState;
 import fr.inria.spirals.repairnator.ScannedBuildStatus;
 import fr.inria.spirals.repairnator.process.step.*;
@@ -34,7 +33,6 @@ public class ProjectInspector {
     private boolean autoclean;
     private String m2LocalPath;
     protected List<AbstractDataSerializer> serializers;
-    private LauncherMode mode;
     private List<URL> repairClassPath;
     private File[] repairSourceDir;
     private boolean isReproducedAsFail;
@@ -42,7 +40,7 @@ public class ProjectInspector {
     protected boolean previousBuildFlag;
 
     public ProjectInspector(BuildToBeInspected buildToBeInspected, String workspace, List<AbstractDataSerializer> serializers,
-                            String nopolSolverPath, boolean push, LauncherMode mode) {
+                            String nopolSolverPath, boolean push) {
         this.buildToBeInspected = buildToBeInspected;
         this.state = ProjectState.NONE;
         this.workspace = workspace;
@@ -54,7 +52,6 @@ public class ProjectInspector {
         this.stepErrors = new HashMap<String, List<String>>();
         this.autoclean = false;
         this.serializers = serializers;
-        this.mode = mode;
     }
 
     public List<URL> getRepairClassPath() {
@@ -143,10 +140,6 @@ public class ProjectInspector {
 
     public NopolRepair getNopolRepair() {
         return nopolRepair;
-    }
-
-    public LauncherMode getMode() {
-        return mode;
     }
 
     public String toString() {
--- scripts/launch_repairnator.sh
+++ scripts/launch_repairnator.sh
@@ -35,13 +35,13 @@ export M2_HOME=/opt/apache-maven-3.3.9
 TOOLS_PATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar
 
 LOG_FILE=$HOME_REPAIR/logs/output_`date "+%Y-%m-%d_%H%M"`.log
+PROJECT_LIST_PATH=$HOME_REPAIR/scripts/project_list.txt
+OUTPUT_PATH=/var/www/html/repairnator/
+WORKSPACE_PATH=$HOME_REPAIR/workspace/
 NB_HOURS=6
 Z3_PATH=$HOME_REPAIR/github/nopol/nopol/lib/z3/z3_for_linux
-WORKSPACE_PATH=$HOME_REPAIR/workspace/
-OUTPUT_PATH=/var/www/html/repairnator/
-PROJECT_LIST_PATH=$HOME_REPAIR/scripts/project_list.txt
 
-REPAIR_ARGS="-m slug -i $PROJECT_LIST_PATH -o $OUTPUT_PATH -w $WORKSPACE_PATH -z $Z3_PATH -p -l $NB_HOURS --clean"
+REPAIR_ARGS="-m repair -f slug -i $PROJECT_LIST_PATH -o $OUTPUT_PATH -w $WORKSPACE_PATH -l $NB_HOURS -p --clean -z $Z3_PATH"
 
 cd $WORKSPACE_PATH
 java -Xmx1g -Xms1g -cp $TOOLS_PATH:$REPAIR_JAR fr.inria.spirals.repairnator.Launcher $REPAIR_ARGS &> $LOG_FILE
