ca7679b69d998677ae4c027d3ce621d6a924e28e
Simon Urli <simon.urli@inria.fr>
2017-03-14 13:34:33 +0100

    Add new variable to manage scanner hour limits

--- repairnator/scripts/set_env_variable.sh
+++ repairnator/scripts/set_env_variable.sh
@@ -15,3 +15,5 @@ export REPAIR_PROJECT_LIST_PATH=$HOME_REPAIR/scripts/project_list.txt
 export REPAIR_OUTPUT_PATH=/var/www/html/repairnator/
 export REPAIR_DOCKER_IMG_DIR=$HOME_REPAIR/dockerImage/
 export GOOGLE_SECRET_PATH=$HOME_REPAIR/client_secret.json
+export SCANNER_NB_HOURS=4
+export SCANNER_MODE=repair
