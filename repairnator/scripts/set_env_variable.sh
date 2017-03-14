772a9024dd3581b660f3cd001312283f7bf378f0
Simon Urli <simon.urli@inria.fr>
2017-03-14 14:33:09 +0100

    Add a missing variable

--- repairnator/scripts/set_env_variable.sh
+++ repairnator/scripts/set_env_variable.sh
@@ -12,19 +12,21 @@ export GOOGLE_SECRET_PATH=$HOME_REPAIR/client_secret.json
 export SCANNER_NB_HOURS=4
 export SCANNER_MODE=repair
 
+export REPAIRNATOR_GH_REPO_PATH=$HOME_REPAIR/github/librepair/repairnator
+
 export DOCKER_VERSION=`date "+%Y-%m-%d_%H%M"`
 export DOCKER_TAG=repairnator/pipeline:$DOCKER_VERSION
 
 export REPAIRNATOR_RUN_DIR=$HOME_REPAIR/bin/`date "+%Y-%m-%d_%H%M"`
 export REPAIRNATOR_DOCKER_DIR=$REPAIRNATOR_RUN_DIR/dockerImage
 
-export REPAIRNATOR_SCANNER_JAR="./repairnator-scanner/target/repairnator-scanner-*-jar-with-dependencies.jar"
+export REPAIRNATOR_SCANNER_JAR="$REPAIRNATOR_GH_REPO_PATH/repairnator-scanner/target/repairnator-scanner-*-jar-with-dependencies.jar"
 export REPAIRNATOR_SCANNER_DEST_JAR=$REPAIRNATOR_RUN_DIR/repairnator-scanner.jar
 
-export REPAIRNATOR_DOCKERPOOL_JAR="./repairnator-dockerpool/target/repairnator-dockerpool-*-jar-with-dependencies.jar"
+export REPAIRNATOR_DOCKERPOOL_JAR="$REPAIRNATOR_GH_REPO_PATH/repairnator-dockerpool/target/repairnator-dockerpool-*-jar-with-dependencies.jar"
 export REPAIRNATOR_DOCKERPOOL_DEST_JAR=$REPAIRNATOR_RUN_DIR/repairnator-dockerpool.jar
 
-export REPAIRNATOR_PIPELINE_JAR="./repairnator-pipeline/target/repairnator-pipeline-*-jar-with-dependencies.jar"
+export REPAIRNATOR_PIPELINE_JAR="$REPAIRNATOR_GH_REPO_PATH/repairnator-pipeline/target/repairnator-pipeline-*-jar-with-dependencies.jar"
 export REPAIRNATOR_PIPELINE_DEST_JAR=$REPAIRNATOR_DOCKER_DIR/repairnator-pipeline.jar
 
 export REPAIRNATOR_BUILD_LIST=$REPAIRNATOR_RUN_DIR/list_build.txt
