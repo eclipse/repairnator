11a65f1f138bcc11342e05bf71c92e8242d79986
Simon Urli <simon.urli@gmail.com>
2017-03-15 15:38:38 +0100

    Move config part and change launch_repairnator to be able to accept a path of build list as an argument. (#122)
    Fix #116

--- repairnator/scripts/launch_repairnator.sh
+++ repairnator/scripts/launch_repairnator.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+SKIP_SCAN=0
+if [ "$#" -eq 1 ]; then
+    if [ ! -f $1 ]; then
+        echo "The list of build id must be an existing file ($1 not found)"
+        exit -1
+    else
+        SKIP_SCAN=1
+    fi
+fi
+
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 echo "Set environment variables"
 source $SCRIPT_DIR/set_env_variable.sh
+
+if [ "$SKIP_SCAN" -eq 1 ]; then
+    REPAIRNATOR_BUILD_LIST=$1
+fi
 
 echo "Create log directory: $LOG_DIR"
 mkdir $LOG_DIR
@@ -26,8 +40,10 @@ mkdir $REPAIRNATOR_DOCKER_DIR
 cp $REPAIRNATOR_PIPELINE_JAR $REPAIRNATOR_PIPELINE_DEST_JAR
 cp -r $REPAIR_DOCKER_IMG_DIR/. $REPAIRNATOR_DOCKER_DIR
 
-echo "Start to scan projects for builds (dest file: $REPAIRNATOR_BUILD_LIST)..."
-java -jar $REPAIRNATOR_SCANNER_DEST_JAR -m $SCANNER_MODE -g $GOOGLE_SECRET_PATH -l $SCANNER_NB_HOURS -i $REPAIR_PROJECT_LIST_PATH -o $REPAIRNATOR_BUILD_LIST -d &> $LOG_DIR/scanner.log
+if [ "$SKIP_SCAN" -eq 0 ]; then
+    echo "Start to scan projects for builds (dest file: $REPAIRNATOR_BUILD_LIST)..."
+    java -jar $REPAIRNATOR_SCANNER_DEST_JAR -m $SCANNER_MODE -g $GOOGLE_SECRET_PATH -l $SCANNER_NB_HOURS -i $REPAIR_PROJECT_LIST_PATH -o $REPAIRNATOR_BUILD_LIST -d &> $LOG_DIR/scanner.log
+fi
 
 NB_LINES=`wc -l $REPAIRNATOR_BUILD_LIST`
 
