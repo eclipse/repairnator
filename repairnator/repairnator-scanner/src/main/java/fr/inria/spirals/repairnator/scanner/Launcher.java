0fd5ae4027e0244e568aa3331e56aab82e8fca11
Simon Urli <simon.urli@inria.fr>
2017-03-22 16:37:57 +0100

    Fix errors with Launcher.java

--- repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/Launcher.java
+++ repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/Launcher.java
@@ -164,7 +164,7 @@ public class Launcher {
         opt2.setHelp("Specify the path to the JSON google secret for serializing.");
         this.jsap.registerParameter(opt2);
 
-        new FlaggedOption("runId");
+        opt2 = new FlaggedOption("runId");
         opt2.setLongFlag("runId");
         opt2.setStringParser(JSAP.STRING_PARSER);
         opt2.setHelp("Specify run id for the scanner.");
