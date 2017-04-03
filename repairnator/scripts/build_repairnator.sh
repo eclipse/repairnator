6bfa70b80e80d89fcb396797b59eaadc219ea55f
Simon Urli <simon.urli@inria.fr>
2017-04-03 17:49:17 +0200

    Add capacity to create a runnable jar for repairnator-core. Remove system dependent variable in shell script.

--- repairnator/scripts/build_repairnator.sh
+++ repairnator/scripts/build_repairnator.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export M2_HOME=/opt/apache-maven-3.3.9
-export PATH=$PATH:$M2_HOME/bin
-
 if [ -z "$HOME_REPAIR" ]; then
     echo "The variable HOME_REPAIR must be set."
     exit -1
