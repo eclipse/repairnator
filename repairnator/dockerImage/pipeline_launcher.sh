91ab3e8558994af1bc8b14a1e26ca81dddf6c718
Simon Urli <simon.urli@inria.fr>
2017-04-07 16:21:24 +0200

    Fix docker script. Create script to update date in mongo db.

--- repairnator/dockerImage/pipeline_launcher.sh
+++ repairnator/dockerImage/pipeline_launcher.sh
@@ -15,7 +15,7 @@ function ca {
   fi
 }
 
-args="`ca -g $GOOGLE_ACCESS_TOKEN``ca --spreadsheet $SPREADSHEET_ID``ca --dbhost $MONGODB_HOME``ca --dbname $MONGODB_NAME``ca --pushurl $PUSH_URL``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
+args="`ca -g $GOOGLE_ACCESS_TOKEN``ca --spreadsheet $SPREADSHEET_ID``ca --dbhost $MONGODB_HOST``ca --dbname $MONGODB_NAME``ca --pushurl $PUSH_URL``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
 
 echo "Execute pipeline with following supplementary args: $args"
 java -cp $JAVA_HOME/lib/tools.jar:repairnator-pipeline.jar -Dlogback.configurationFile=/root/logback.xml fr.inria.spirals.repairnator.pipeline.Launcher -m $REPAIR_MODE -d -b $BUILD_ID --runId $RUN_ID -o $OUTPUT $args
---
+++ resources/mongo_update.js
@@ -0,0 +1,44 @@
+// Thanks by A. Veuiller for this script! \o/
+
+function toIsodate(crop, fieldName, collectionName) {
+    // regexp format match date like: 07/04/17 13:26
+    var re = new RegExp('^([0-9]{2})/([0-9]{2})/([0-9]{2})\\s([0-9]{2}:[0-9]{2})$','g');
+    var update = {$set: {}};
+
+    var fieldValue = crop[fieldName];
+
+    if (typeof fieldValue == "string" && fieldValue != "") {
+        var replaceDate = fieldValue.replace(re, '20$3-$2-$1T$4:00Z');
+        update["$set"][fieldName] = new Date(replaceDate);
+        update["$set"][fieldName+"Str"] = fieldValue;
+
+        db[collectionName].update(
+            {
+                "_id": crop._id // Would be better with $type: "string" but doesn't work for some reason
+            },
+            update
+        );
+
+        print("Date updated!");
+    }
+}
+
+function updateCollection(collectionName, fieldList) {
+
+    db[collectionName].find().forEach(function (crop) {
+        print("### " + crop._id + " ###");
+        print("# Update date String to ISODate");
+
+        fieldList.forEach(function (fieldName) {
+            toIsodate(crop, fieldName, collectionName);
+        });
+        print();
+    });
+}
+
+updateCollection("scanner", ["dateBegin", "dateLimit"]);
+updateCollection("inspector", ["buildFinishedDate", "buildReproductionDate"]);
+updateCollection("endprocess", ["beginDate", "endDate"]);
+updateCollection("nopol", ["nopolDateEnd"]);
+updateCollection("times", ["buildReproductionDate"]);
+updateCollection("treatedbuild", ["dateReproducedBuild"]);
