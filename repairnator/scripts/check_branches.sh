e23617a868b1f6ecd44dfcd884090fbab2dd1a5c
Simon Urli <simon.urli@inria.fr>
2017-05-04 13:52:38 +0200

    Change again the script for checking branches

--- repairnator/scripts/check_branches.sh
+++ repairnator/scripts/check_branches.sh
@@ -13,6 +13,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+MAVEN_TEST_ARGS="-Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true"
+
 mkdir $TMP_GIT_DIR
 cd $TMP_GIT_DIR
 git clone $REPO $TMP_GIT_DIR
@@ -24,7 +26,7 @@ then
 fi
 
 echo "Analyze started: `date "+%Y-%m-%d_%H%M%S"`" > $DEST
-echo "Considered repository: $REPO" > $DEST
+echo "Considered repository: $REPO" >> $DEST
 
 git for-each-ref --shell --format="branchname=%(refname:strip=3)" refs/remotes | \
 while read entry
@@ -39,18 +41,14 @@ do
         git checkout -q $branchname
 
         bugCommitId=`git log --format=format:%H --grep="Bug commit"`
-        echo "Checking out the bug commit $bugCommitId"
+        patchCommitId=`git log --format=format:%H --grep="Human patch"`
+
+        echo "Checking out the bug commit: $bugCommitId"
         git log --format=%B -n 1 $bugCommitId
 
         git checkout -q $bugCommitId
-        mvn -q install -DskipTests=true
-        if [ "$?" -ne 0 ]; then
-            >&2 echo -e "$RED Error while building the project with bug for branch $branchname $NC"
-            echo "$branchname [FAILURE] (bug build)" >> $DEST
-            continue
-        fi
 
-        mvn -q -Dsurefire.printSummary=false test
+        mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
 
         if [ "$?" -eq 0 ]; then
             >&2 echo -e "$RED Error while reproducing the bug for branch $branchname $NC"
@@ -58,19 +56,12 @@ do
             continue
         fi
 
-        patchCommitId=`git log --format=format:%H --grep="Human patch"`
-        echo "Checking out the patch commit $patchCommitId"
+        echo "Checking out the patch commit: $patchCommitId"
         git log --format=%B -n 1 $patchCommitId
 
         git checkout -q $patchCommitId
-        mvn -q clean install -DskipTests=true
-        if [ "$?" -ne 0 ]; then
-            >&2 echo -e "$RED Error while building the project with patch for branch $branchname $NC"
-            echo "$branchname [FAILURE] (patch build)" >> $DEST
-            continue
-        fi
 
-        mvn -q -Dsurefire.printSummary=false test
+        mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
 
         if [ "$?" -ne 0 ]; then
             >&2 echo -e "$RED Error while reproducing the passing build for branch $branchname $NC"
