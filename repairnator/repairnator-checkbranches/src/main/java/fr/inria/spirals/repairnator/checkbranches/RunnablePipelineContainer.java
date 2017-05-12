6b0a46b69bdda3d3efed3a91c442437b1f6c0503
Simon Urli <simon.urli@gmail.com>
2017-05-12 14:00:06 +0200

    Using a pool of docker container for checkbranches (#274)
    * Working on a docker pool for checkbranches
    
    * Finish to create the pool for checkbranches
    
    * Create a new docker image for checkbranches stuff. Rename pipeline docker image to distinguish
    
    * Create a dedicated script for checkbranches using a pool of docker container

---
+++ repairnator/repairnator-checkbranches/src/main/java/fr/inria/spirals/repairnator/checkbranches/RunnablePipelineContainer.java
@@ -0,0 +1,104 @@
+package fr.inria.spirals.repairnator.checkbranches;
+
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+import com.spotify.docker.client.messages.ContainerExit;
+import com.spotify.docker.client.messages.HostConfig;
+import fr.inria.spirals.repairnator.Utils;
+import fr.inria.spirals.repairnator.config.RepairnatorConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by urli on 14/03/2017.
+ */
+public class RunnablePipelineContainer implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RunnablePipelineContainer.class);
+    private String imageId;
+    private String branchName;
+    private String output;
+    private RepairnatorConfig repairnatorConfig;
+    private boolean skipDelete;
+    private String repository;
+
+
+    public RunnablePipelineContainer(String imageId, String repository, String branchName, String output, boolean skipDelete) {
+        this.imageId = imageId;
+        this.branchName = branchName;
+        this.output = output;
+        this.repairnatorConfig = RepairnatorConfig.getInstance();
+        this.skipDelete = skipDelete;
+        this.repository = repository;
+    }
+
+    @Override
+    public void run() {
+        String containerId = null;
+        DockerClient docker = Launcher.docker;
+        try {
+            LOGGER.info("Start to run check container for branch "+branchName);
+
+            String containerName = "repairnator-checkbranch_"+ Utils.formatFilenameDate(new Date());
+
+            String[] envValues = new String[] {
+                "BRANCH_NAME="+this.branchName,
+                "REPOSITORY="+this.repository
+            };
+
+            Map<String,String> labels = new HashMap<>();
+            labels.put("name",containerName);
+            HostConfig hostConfig = HostConfig.builder().appendBinds(this.output+":/tmp/result.txt").build();
+            ContainerConfig containerConfig = ContainerConfig.builder()
+                    .image(imageId)
+                    .env(envValues)
+                    .hostname(Utils.getHostname())
+                    .hostConfig(hostConfig)
+                    .labels(labels)
+                    .build();
+
+            LOGGER.info("Create the container: "+containerName);
+            ContainerCreation container = docker.createContainer(containerConfig);
+
+            containerId = container.id();
+
+            LOGGER.info("Start the container: "+containerName);
+            docker.startContainer(container.id());
+
+            ContainerExit exitStatus = docker.waitContainer(containerId);
+
+            LOGGER.info("The container has finished with status code: "+exitStatus.statusCode());
+
+            if (!skipDelete && exitStatus.statusCode() == 0) {
+                LOGGER.info("Container will be removed.");
+                docker.removeContainer(containerId);
+            }
+            LOGGER.info("TREATED Docker container : "+containerId);
+        } catch (InterruptedException e) {
+            LOGGER.error("Error while running the container for branch name "+branchName, e);
+            killDockerContainer(docker, containerId);
+        } catch (DockerException e) {
+            LOGGER.error("Error while creating or running the container for branch name "+branchName, e);
+        }
+        Launcher.submittedRunnablePipelineContainers.remove(this);
+    }
+
+    private void killDockerContainer(DockerClient docker, String containerId) {
+        LOGGER.info("Killing docker container: "+containerId);
+        try {
+            docker.killContainer(containerId);
+            docker.removeContainer(containerId);
+        } catch (DockerException|InterruptedException e) {
+            LOGGER.error("Error while killing docker container "+containerId, e);
+        }
+
+    }
+
+}
---
+++ repairnator/scripts/check_branches.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./check_branches.sh <input branch names> <output result>"
+    exit 2
+fi
+
+# Use to create args in the command line for optionnal arguments
+function ca {
+  if [ -z "$2" ];
+  then
+      echo ""
+  else
+    if [ "$2" == "null" ];
+    then
+        echo ""
+    else
+        echo "$1 $2 "
+    fi
+  fi
+}
+
+
+if [ ! -f $1 ]; then
+    echo "The list of input branches must be an existing file ($1 not found)"
+    exit -1
+fi
+
+if [ -f $2 ]; then
+    echo "The output file already exists, please specify a path for a new file to create ($2 already exists)"
+    exit -1
+fi
+
+INPUT=$1
+OUTPUT=$2
+
+touch $OUTPUT
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+echo "Set environment variables"
+source $SCRIPT_DIR/set_env_variable.sh
+
+echo "Create log directory: $LOG_DIR"
+mkdir $LOG_DIR
+
+RUN_ID=`date "+%Y-%m-%d_%H%M%S"`
+
+echo "Start building a new version of repairnator"
+$SCRIPT_DIR/build_repairnator.sh
+
+if [[ $? != 0 ]]
+then
+   echo "Error while building a new version of repairnator"
+   exit -1
+fi
+
+echo "Copy jar and prepare docker image"
+mkdir $REPAIRNATOR_RUN_DIR
+
+cp $REPAIRNATOR_CHECKBRANCHES_JAR $REPAIRNATOR_CHECKBRANCHES_DEST_JAR
+
+echo "Pull the docker machine (name: $DOCKER_CHECKBRANCHES_TAG)..."
+docker pull $DOCKER_CHECKBRANCHES_TAG
+
+echo "Analyze started: `date "+%Y-%m-%d_%H%M%S"`" > $OUTPUT
+echo "Considered repository: $CHECK_BRANCH_REPOSITORY" >> $OUTPUT
+
+echo "Launch docker pool checkbranches ..."
+args="`ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
+if [ "$NOTIFY_ENDPROCESS" -eq 1 ]; then
+    args="$args --notifyEndProcess"
+fi
+
+echo "Supplementary args for docker pool checkbranches $args"
+java -jar $REPAIRNATOR_CHECKBRANCHES_DEST_JAR -t $NB_THREADS -n $DOCKER_CHECKBRANCHES_TAG -i $INPUT -o $OUTPUT -g $DAY_TIMEOUT --runId $RUN_ID $args &> $LOG_DIR/checkbranches.log
+
+echo "Docker pool checkbranches finished, delete the run directory ($REPAIRNATOR_RUN_DIR)"
+rm -rf $REPAIRNATOR_RUN_DIR
+
+echo "Analyze finished: `date "+%Y-%m-%d_%H%M%S"`" >> $OUTPUT
