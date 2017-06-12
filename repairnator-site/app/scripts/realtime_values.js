ba8ed9719711eb6c6201f98c87660cec053317cd
Simon Urli <simon.urli@inria.fr>
2017-06-12 15:10:17 +0200

    Change website to order column for dashboard #279

--- repairnator-site/app/scripts/realtime_values.js
+++ repairnator-site/app/scripts/realtime_values.js
@@ -1,27 +1,40 @@
 $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/', function (datas) {
   var htmlElement = $('#tablerealtime');
 
+  var fieldNames = [
+    {id:'buildFinishedDateStr', readable: 'Original date'},
+    {id: 'buildReproductionDateStr', readable: 'Date of the reproduction'},
+    {id: 'buildId', readable: 'Build ID'},
+    {id: 'repositoryName', readable: 'Github Repository'},
+    {id: 'status', readable: 'Status'},
+    {id: 'prNumber', readable: 'Pull Request ID'},
+    {id: 'travisURL', readable: 'URL of Travis build'},
+    {id: 'typeOfFailures', readable: 'Type of failures'}
+  ];
+
   var headersDisplayed = false;
   datas.forEach(function (data) {
     var row = $('<tr></tr>');
     htmlElement.append(row);
 
-    var fieldName;
     if (!headersDisplayed) {
-      for (fieldName in data) {
+      fieldNames.forEach(function (fieldName) {
         var th = $('<th></th>');
-        th.text(fieldName);
+        th.text(fieldName.readable);
 
         row.append(th);
-      }
+      });
 
       row = $('<tr></tr>');
       htmlElement.append(row);
       headersDisplayed = true;
     }
 
-    for (fieldName in data) {
+    fieldNames.forEach(function (column) {
+      var fieldName = column.id;
       var td = $('<td></td>');
+      var dataValue = data[fieldName];
+
       if (fieldName == 'status') {
         if (data[fieldName] == 'PATCHED') {
           row.addClass('success');
@@ -29,9 +42,14 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/', fun
           row.addClass('warning');
         }
       }
-      td.text(data[fieldName]);
+
+      if (fieldName == 'travisURL') {
+        dataValue = '<a href="'+dataValue+'">'+dataValue+'</a>';
+      }
+
+      td.html(dataValue);
 
       row.append(td);
-    }
+    });
   });
 });
