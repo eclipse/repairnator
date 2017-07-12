58932534b33591d6227c8f2bb8078f16f3663aea
Simon Urli <simon.urli@inria.fr>
2017-07-12 18:00:36 +0200

    Improve title in graph website

--- repairnator-site/app/scripts/statuses_pie.js
+++ repairnator-site/app/scripts/statuses_pie.js
@@ -1,5 +1,9 @@
 $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/statusStats', function (data) {
   var htmlElement = $('<div style="float: left; width: 49%;"></div>');
+
+  var total = 0;
+  data.forEach(element => {total += element.counted});
+
   $('#charts').append(htmlElement);
   Highcharts.chart({
     chart: {
@@ -10,7 +14,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
       renderTo: htmlElement[0]
     },
     title: {
-      text: 'Build statuses (all times)'
+      text: 'Build statuses (all times - '+total+' builds)'
     },
     tooltip: {
       pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -43,6 +47,9 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
 
 $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/statusStats/2', function (data) {
 
+  var total = 0;
+  data.forEach(element => {total += element.counted});
+
   var htmlElement = $('<div style="float: right; width: 49%;"></div>');
   $('#charts').append(htmlElement);
   Highcharts.chart({
@@ -54,7 +61,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
       renderTo: htmlElement[0]
     },
     title: {
-      text: 'Build statuses (last 2 weeks)'
+      text: 'Build statuses (last 2 weeks - '+total+' builds)'
     },
     tooltip: {
       pointFormat: '{series.name}: <b>{point.percentage:.1f}% ({point.y})</b>'
