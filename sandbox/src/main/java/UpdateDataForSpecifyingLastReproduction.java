7c8e65416959cb22f2bcdee126078dc724e3af72
Simon Urli <simon.urli@gmail.com>
2017-09-04 17:58:45 +0200

    Add sandbox project (#330)
    * Add sandbox project
    
    * Working on a way to score repairnator.json files on their suspiciousness to refer to a pair bug/patch
    
    * Fix errors with tool to evaluate potential bugs
    
    * Try to improve computing a score for patched commits
    
    * Now compute score and also export it in csv
    
    * Quick fix in order to avoid NPE sometimes
    
    * Change some things to compute a score for potential bugs
    
    * Try to fix error related to M2_HOME

---
+++ sandbox/src/main/java/UpdateDataForSpecifyingLastReproduction.java
@@ -0,0 +1,70 @@
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
+import fr.inria.spirals.repairnator.serializer.mongodb.MongoConnection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Sorts.descending;
+import static com.mongodb.client.model.Sorts.orderBy;
+
+/**
+ * Created by urli on 29/08/2017.
+ */
+public class UpdateDataForSpecifyingLastReproduction {
+
+    public static void main(String[] args) {
+        String dbConnectionUrl = args[0];
+        String dbName = args[1];
+        String collectionName = args[2];
+
+        MongoConnection mongoConnection = new MongoConnection(dbConnectionUrl, dbName);
+        MongoDatabase database = mongoConnection.getMongoDatabase();
+        MongoCollection collection = database.getCollection(collectionName);
+
+        Calendar limitDateMay = Calendar.getInstance();
+        limitDateMay.set(2017, Calendar.MAY, 01);
+
+        final List<ObjectId> updatedDocs = new ArrayList<>();
+
+        Set<Integer> ids = new HashSet<>();
+
+        Block<Document> block = new Block<Document>() {
+
+            @Override
+            public void apply(Document document) {
+                ObjectId documentId = document.getObjectId("_id");
+
+                Object pBuildId = document.get("previousBuildId");
+
+                if (pBuildId instanceof Integer) {
+                    int previousBuildId = document.getInteger("previousBuildId", -1);
+                    if (previousBuildId != -1) {
+                        boolean lastReproducedBuggyBuild = !ids.contains(previousBuildId);
+                        ids.add(previousBuildId);
+
+                        document.append("lastReproducedBuggyBuild", lastReproducedBuggyBuild);
+                        collection.replaceOne(eq("_id", documentId), document, new UpdateOptions().upsert( true ));
+                        updatedDocs.add(documentId);
+                    }
+                }
+
+            }
+        };
+
+        collection.find().sort(orderBy(descending("buildReproductionDate"))).forEach(
+                block
+        );
+
+        System.out.println("Updated docs: "+updatedDocs.size());
+
+    }
+}
