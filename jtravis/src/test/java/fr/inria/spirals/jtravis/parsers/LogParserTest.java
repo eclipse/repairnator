e5254c5f676b961b118a1bd9c83262a4a5a24591
vianneyPayelle <vianney3003@gmail.com>
2017-09-20 18:36:16 +0200

    Testing new regular expression for build logs

--- jtravis/src/test/java/fr/inria/spirals/jtravis/parsers/LogParserTest.java
+++ jtravis/src/test/java/fr/inria/spirals/jtravis/parsers/LogParserTest.java
@@ -3,8 +3,10 @@ package fr.inria.spirals.jtravis.parsers;
 import fr.inria.spirals.jtravis.TestUtils;
 import fr.inria.spirals.jtravis.entities.*;
 import fr.inria.spirals.jtravis.helpers.BuildHelper;
+import javafx.scene.shape.Path;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
@@ -111,6 +113,43 @@ public class LogParserTest {
 
         assertTrue(build != null);
         assertEquals(true, result);
+    }
+
+    @Test
+    public void testLogParserRecognizeLogAsGradleLog() throws IOException {
+        String path = "./src/test/resources/UnkownBuildTool/Gradle/";
+
+        String fileContent = TestUtils.readFile(path);
+        LogParser parser = new LogParser(fileContent);
+
+        assertEquals(BuildTool.GRADLE, parser.getBuildTool());
+    }
+
+    @Test
+    public void testLogParserRecognizeALogAsMavenLog() throws IOException {
+        String path = "./src/test/resources/UnkownBuildTool/Maven/BUILD-SUCCESS/";
+        //String path = "./src/test/resources/maven-logs/";
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+
+        for(File file : files) {
+            if(file.isFile()) {
+                String fileContent = TestUtils.readFile(file.getPath());
+                LogParser parser = new LogParser(fileContent);
+                assertEquals(BuildTool.MAVEN, parser.getBuildTool());
+            }
+            else if (file.isDirectory()) {
+                for(File file2 : file.listFiles()) {
+                    if (file2.isFile()) {
+                        String fileContent = TestUtils.readFile(file2.getPath());
+                        LogParser parser = new LogParser(fileContent);
+                        assertEquals(BuildTool.MAVEN, parser.getBuildTool());
+                    }
+                }
+            }
+        }
+
+
     }
 
 }
