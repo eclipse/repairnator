b9f2caf80210b21e0d042483b9e64cb8ba1c2808
Simon Urli <simon.urli@inria.fr>
2017-09-25 18:26:41 +0200

    Fix bug with last 2 weeks & add a new graph for the whole scanner information. #343

--- repairnator-site/app/scripts/scanner_timechart.js
+++ repairnator-site/app/scripts/scanner_timechart.js
@@ -33,7 +33,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/scanners/weeksDat
 
       if (previousValue.dayLimit === currentValue.dayLimit) {
         for (var attr in previousValue) {
-          if (attr.indexOf('total') != -1) {
+          if (attr.indexOf('total') != -1 && attr.indexOf('totalRepoNumber') == -1) {
             previousValue[attr] += currentValue[attr];
           }
         }
@@ -83,6 +83,15 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/scanners/weeksDat
 
     series: [
       {
+        name: 'totalRepoNumber',
+        data: reducedData.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalRepoNumber
+          ]
+        })
+      },
+      {
         name: 'totalScannedBuilds',
         data: reducedData.map( function (d) {
           return [
@@ -121,6 +130,127 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/scanners/weeksDat
       {
         name: 'totalJavaFailingBuildsWithFailingTests',
         data: reducedData.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalJavaFailingBuildsWithFailingTests
+          ]
+        })
+      }
+    ]
+  });
+});
+
+$.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/scanners/list', function (data) {
+  var htmlElement = $('<div></div>');
+  $('#charts').append(htmlElement);
+
+  /*
+   {
+   '_id': '58e22490cefc9665e4354cde',
+   'hostname': 'repairnator',
+   'dateBegin': '2017-04-03T12:00:00.000Z',
+   'dateLimit': '2017-04-03T08:00:00.000Z',
+   'dayLimit': '03/04/2017',
+   'totalRepoNumber': 1611,
+   'totalRepoUsingTravis': 1601,
+   'totalScannedBuilds': 205,
+   'totalJavaBuilds': 198,
+   'totalJavaPassingBuilds': 152,
+   'totalJavaFailingBuilds': 27,
+   'totalJavaFailingBuildsWithFailingTests': 8,
+   'totalPRBuilds': 52,
+   'duration': '0:31:4',
+   'runId': '92d87100-e392-4b7c-8f49-2789af76ef7c',
+   'dateBeginStr': '03/04/17 12:00',
+   'dateLimitStr': '03/04/17 08:00'
+   },
+   */
+
+  Highcharts.chart({
+    chart: {
+      type: 'spline',
+      renderTo: htmlElement[0]
+    },
+    title: {
+      text: 'Number of scanned builds'
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { // don't display the dummy year
+        day:'%A, %b %e',
+      },
+      title: {
+        text: 'Date'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Number of builds'
+      },
+      min: 0
+    },
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br>',
+      pointFormat: '{point.x:%A, %b %e}: {point.y} builds'
+    },
+
+    plotOptions: {
+      spline: {
+        marker: {
+          enabled: true
+        }
+      }
+    },
+
+    series: [
+      {
+        name: 'totalRepoNumber',
+        data: data.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalRepoNumber
+          ]
+        })
+      },
+      {
+        name: 'totalScannedBuilds',
+        data: data.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalScannedBuilds
+          ]
+        })
+      },
+      {
+        name: 'totalJavaBuilds',
+        data: data.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalJavaBuilds
+          ]
+        })
+      },
+      {
+        name: 'totalJavaPassingBuilds',
+        data: data.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalJavaPassingBuilds
+          ]
+        })
+      },
+      {
+        name: 'totalJavaFailingBuilds',
+        data: data.map( function (d) {
+          return [
+            Date.parse(d.dateLimit),
+            d.totalJavaFailingBuilds
+          ]
+        })
+      },
+      {
+        name: 'totalJavaFailingBuildsWithFailingTests',
+        data: data.map( function (d) {
           return [
             Date.parse(d.dateLimit),
             d.totalJavaFailingBuildsWithFailingTests
