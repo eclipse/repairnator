4dd64e065acf5427fbe0c262d8196b1bc4cb04fe
Simon Urli <simon.urli@inria.fr>
2017-10-09 17:07:57 +0200

    Fix issue with PatchNotifier to send mail for both NPEFix and Nopol

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/notifier/PatchNotifier.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/notifier/PatchNotifier.java
@@ -1,6 +1,7 @@
 package fr.inria.spirals.repairnator.notifier;
 
 import fr.inria.lille.repair.common.patch.Patch;
+import fr.inria.spirals.jtravis.entities.Job;
 import fr.inria.spirals.repairnator.Utils;
 import fr.inria.spirals.repairnator.notifier.engines.NotifierEngine;
 import fr.inria.spirals.repairnator.process.inspectors.JobStatus;
@@ -14,19 +15,84 @@ import java.util.List;
  * Created by urli on 30/03/2017.
  */
 public class PatchNotifier extends AbstractNotifier {
-    private boolean alreadyNotified;
+    private boolean alreadyNotifiedForNopol;
+    private boolean alreadyNotifiedForNPEFix;
+    private boolean alreadyNotifiedForAstor;
 
     public PatchNotifier(List<NotifierEngine> engines) {
         super(engines);
-        this.alreadyNotified = false;
+    }
+
+    public String notifyForNopol(JobStatus status) {
+        if (alreadyNotifiedForNopol) {
+            return "";
+        }
+
+        int i = 1;
+        int totalFailure = status.getNopolInformations().size();
+        String details = "Nopol Generated Patches: \n\n";
+
+        for (NopolInformation nopolInformation : status.getNopolInformations()) {
+            FailureLocation location = nopolInformation.getLocation();
+            details += "\t Failure #"+i+" on "+totalFailure+"\n" +
+                    "\t\t Concerned class: "+location.getClassName()+" ("+location.getNbFailures()+" failures / "+location.getNbErrors()+" errors)\n";
+
+            for (int j = 0; j < Math.min(10, nopolInformation.getPatches().size()); j++) {
+                Patch patch = nopolInformation.getPatches().get(j);
+                details += "\t\t Proposed patch #"+j+" in "+patch.getSourceLocation().toString()+" : "+patch.asString()+"\n";
+            }
+
+            details += "\n\n";
+            i++;
+        }
+
+        this.alreadyNotifiedForNopol = true;
+        return details;
+    }
+
+    public String notifyForNPEFix(JobStatus status) {
+        if (alreadyNotifiedForNPEFix) {
+            return "";
+        }
+
+        int i = 1;
+        int npePatches = status.getNpeFixPatches().size();
+
+        String details = "NPEfix generated patches: \n\n";
+        for (String diff : status.getNpeFixPatches()) {
+            details += "\tPatch #"+i+" on "+npePatches+"\n";
+            details += "\tDiff: "+diff+"\n\n";
+            i++;
+        }
+
+        this.alreadyNotifiedForNPEFix = true;
+        return details;
+    }
+
+    public String notifityForAstor(JobStatus status) {
+        if (alreadyNotifiedForAstor) {
+            return "";
+        }
+
+        int i = 1;
+        int astorPatches = status.getAstorPatches().size();
+
+        String details = "Astor generated patches: \n\n";
+        for (String diff : status.getAstorPatches()) {
+            details += "\tPatch #"+i+" on "+astorPatches+"\n";
+            details += "\tDiff: "+diff+"\n\n";
+            i++;
+        }
+
+        this.alreadyNotifiedForAstor = true;
+        return details;
     }
 
     public void observe(ProjectInspector inspector) {
         JobStatus status = inspector.getJobStatus();
-        if (!alreadyNotified && status.isHasBeenPatched()) {
+
+        if (status.isHasBeenPatched()) {
             String subject = "Patched build: "+inspector.getBuggyBuild().getId()+" - "+inspector.getBuggyBuild().getRepository().getSlug();
-
-
 
             String text = "Hurray !\n\n" +
                     "Patch(es) has been found for the following build: "+ Utils.getTravisUrl(inspector.getBuggyBuild().getId(), inspector.getBuggyBuild().getRepository().getSlug())+".\n";
@@ -37,46 +103,24 @@ public class PatchNotifier extends AbstractNotifier {
 
             text += "You may find several information on the following about those patches: \n";
 
-            int totalPatches = 0;
-            String details = "";
-            if (status.getNopolInformations() != null && !status.getNopolInformations().isEmpty()) {
-                int i = 1;
-                int totalFailure = status.getNopolInformations().size();
-                details += "Nopol Generated Patches: \n\n";
-                for (NopolInformation nopolInformation : status.getNopolInformations()) {
-                    totalPatches += nopolInformation.getPatches().size();
 
-                    FailureLocation location = nopolInformation.getLocation();
-                    details += "\t Failure #"+i+" on "+totalFailure+"\n" +
-                            "\t\t Concerned class: "+location.getClassName()+" ("+location.getNbFailures()+" failures / "+location.getNbErrors()+" errors)\n";
+            if (status.getNopolInformations() != null && !status.getNopolInformations().isEmpty() && !this.alreadyNotifiedForNopol) {
+                text += this.notifyForNopol(status);
 
-                    for (int j = 0; j < Math.min(10, nopolInformation.getPatches().size()); j++) {
-                        Patch patch = nopolInformation.getPatches().get(j);
-                        details += "\t\t Proposed patch #"+j+" in "+patch.getSourceLocation().toString()+" : "+patch.asString()+"\n";
-                        totalPatches++;
-                    }
-
-                    details += "\n\n";
-                    i++;
-                }
+                this.notifyEngines("[NOPOL] "+subject, text);
             }
 
-            if (status.getNpeFixPatches() != null && !status.getNpeFixPatches().isEmpty()) {
-                int i = 1;
-                int npePatches = status.getNpeFixPatches().size();
+            if (status.getNpeFixPatches() != null && !status.getNpeFixPatches().isEmpty() && !this.alreadyNotifiedForNPEFix) {
+                text += this.notifyForNPEFix(status);
 
-                details += "NPEfix generated patches: \n\n";
-                for (String diff : status.getNpeFixPatches()) {
-                    details += "\tPatch #"+i+" on "+npePatches+"\n";
-                    details += "\tDiff: "+diff+"\n\n";
-                    totalPatches++;
-                    i++;
-                }
+                this.notifyEngines("[NPEFIX] "+subject, text);
             }
 
-            text += "\t Total patches found: "+totalPatches+".\n" + details;
-            this.notifyEngines(subject, text);
-            this.alreadyNotified = true;
+            if (status.getAstorPatches() != null && !status.getAstorPatches().isEmpty() && !this.alreadyNotifiedForAstor) {
+                text += this.notifityForAstor(status);
+
+                this.notifyEngines("[ASTOR] "+subject, text);
+            }
         }
     }
 }
