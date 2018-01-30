cce87fdda10d9dce2979ba029a3c56b463040883
Simon Urli <simon.urli@inria.fr>
2018-01-30 11:20:47 +0100

    Fix checkstyle

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/AstorRepair.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/AstorRepair.java
@@ -20,12 +20,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Created by urli on 17/08/2017.
