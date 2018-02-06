4854e29143f7371b3f230292cdc06310b0a6569f
Simon Urli <simon.urli@inria.fr>
2018-02-06 11:27:46 +0100

    Update SEIP data

--- resources/SEIP/queries/get-all-reproduced-bugs.js
+++ resources/SEIP/queries/get-all-reproduced-bugs.js
@@ -12,17 +12,9 @@ db.inspector.find({
             }
         },
         {
-            "buildReproductionDate": {
-                $lte: ISODate("2018-01-11T00:00:00.000+0000")
-            }
-        },
-        {
             "status": {
                 $in: ["PATCHED", "test errors", "test failure"]
             }
-        },
-        {
-            "branchURL": null
         }
     ]
-})
+});
