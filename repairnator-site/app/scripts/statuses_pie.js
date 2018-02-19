3ec19df39686b01809f3d593d0b1592555800d55
Simon Urli <simon.urli@gmail.com>
2018-02-19 10:56:30 +0100

    Change website for slides (#411)
    * Change website
    
    * Simplify statuses in website piecharts
    
    * Remove double quotes
    
    * Hide back scanner SEIP

--- repairnator-site/app/scripts/statuses_pie.js
+++ repairnator-site/app/scripts/statuses_pie.js
@@ -1,8 +1,53 @@
+var simplifyStatuses = function (dataArray) {
+  var success = 'Successful Bug Reproduction';
+  var withoutFailure = 'Test without failure';
+  var errorTesting = 'Error when testing';
+  var errorCompiling = 'Error when compiling';
+  var errorCheckout = 'Error when checking out';
+  var errorCloning = 'Error when cloning';
+
+  var statusMap = {
+    'PATCHED': success,
+    'test failure': success,
+    'test errors': success,
+    'NOTBUILDABLE': errorCompiling,
+    'SOURCEDIRNOTCOMPUTED': errorCompiling,
+    'NOTCLONABLE': errorCloning,
+    'NOTFAILING': withoutFailure,
+    'NOTTESTABLE': errorTesting,
+    'TESTABLE': errorTesting,
+    'BUILDNOTCHECKEDOUT': errorCheckout
+  };
+
+  var dataNewName = dataArray.reduce(function (acc, elem) {
+    var status = statusMap[elem._id];
+    if (acc[status] === undefined) {
+      acc[status] = elem.counted;
+    } else {
+      acc[status] += elem.counted;
+    }
+    return acc;
+  }, {});
+
+  var finalArray = Object.keys(dataNewName).reduce(function (acc, id) {
+    acc.push({
+      'name': id,
+      'y': dataNewName[id]
+    });
+    return acc;
+  }, []);
+
+  return finalArray;
+};
+
 $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/statusStats', function (data) {
   var htmlElement = $('<div style="display: inline-block; width: 30%"></div>');
 
   var total = 0;
   data.forEach(element => {total += element.counted});
+
+
+  // return acc.set(item._id, item.counted + acc.get(item._id));
 
   $('#charts').append(htmlElement);
   Highcharts.chart({
@@ -35,12 +80,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
     series: [{
       name: 'Statuses',
       colorByPoint: true,
-      data: data.map(function (d) {
-        return {
-          name: d._id,
-          y: d.counted
-        }
-      })
+      data: simplifyStatuses(data)
     }]
   });
 });
@@ -82,12 +122,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
     series: [{
       name: 'Statuses',
       colorByPoint: true,
-      data: data.map(function (d) {
-        return {
-          name: d._id,
-          y: d.counted
-        }
-      })
+      data: simplifyStatuses(data)
     }]
   });
 });
@@ -129,12 +164,7 @@ $.get('http://repairnator.lille.inria.fr/repairnator-mongo-api/inspectors/status
     series: [{
       name: 'Statuses',
       colorByPoint: true,
-      data: data.map(function (d) {
-        return {
-          name: d._id,
-          y: d.counted
-        }
-      })
+      data: simplifyStatuses(data)
     }]
   });
 });
