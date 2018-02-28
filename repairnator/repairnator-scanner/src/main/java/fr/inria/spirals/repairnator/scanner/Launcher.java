5fb78ac522283480d782318f839e3b2b7ac5b53b
Fer Madeiral <fer.madeiral@gmail.com>
2018-02-28 08:38:35 -0300

    Fix date interval for scanning builds

--- repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/Launcher.java
+++ repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/Launcher.java
@@ -224,14 +224,14 @@ public class Launcher {
         opt2.setShortFlag('f');
         opt2.setLongFlag("lookFromDate");
         opt2.setStringParser(dateStringParser);
-        opt2.setHelp("Specify the initial date to get builds");
+        opt2.setHelp("Specify the initial date to get builds (e.g. 01/01/2017). Note that the search starts from 00:00:00 of the specified date.");
         this.jsap.registerParameter(opt2);
 
         opt2 = new FlaggedOption("lookToDate");
         opt2.setShortFlag('t');
         opt2.setLongFlag("lookToDate");
         opt2.setStringParser(dateStringParser);
-        opt2.setHelp("Specify the final date to get builds");
+        opt2.setHelp("Specify the final date to get builds (e.g. 31/01/2017). Note that the search is until 23:59:59 of the specified date.");
         this.jsap.registerParameter(opt2);
 
         opt2 = new FlaggedOption("googleSecretPath");
@@ -287,6 +287,15 @@ public class Launcher {
         ProjectScanner scanner;
         Date lookFromDate = this.arguments.getDate("lookFromDate");
         Date lookToDate = this.arguments.getDate("lookToDate");
+        if (lookToDate != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(lookToDate);
+            calendar.set(Calendar.HOUR_OF_DAY, calendar.getMaximum(Calendar.HOUR_OF_DAY));
+            calendar.set(Calendar.MINUTE, calendar.getMaximum(Calendar.MINUTE));
+            calendar.set(Calendar.SECOND, calendar.getMaximum(Calendar.SECOND));
+            calendar.set(Calendar.MILLISECOND, calendar.getMaximum(Calendar.MILLISECOND));
+            lookToDate = calendar.getTime();
+        }
         if (lookFromDate != null && lookToDate != null && lookFromDate.before(lookToDate)) {
             scanner = new ProjectScanner(lookFromDate, lookToDate, launcherMode, this.arguments.getString("runId"), this.arguments.getBoolean("skip-failing"));
         } else {
