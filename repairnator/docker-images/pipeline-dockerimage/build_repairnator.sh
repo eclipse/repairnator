9e86f43f5ac44d65e0b85d959a28996e7e9b4654
Fer Madeiral <fer.madeiral@gmail.com>
2018-03-13 12:27:09 -0300

    Just to test

--- repairnator/docker-images/pipeline-dockerimage/build_repairnator.sh
+++ repairnator/docker-images/pipeline-dockerimage/build_repairnator.sh
@@ -5,11 +5,12 @@ set -e
 cd /root
 mkdir github
 cd github
-git clone --recursive https://github.com/Spirals-Team/librepair.git
+git clone --recursive https://github.com/fermadeiral/repairnator.git
 
 echo "LibRepair repository cloned."
 
-cd librepair/repairnator
+cd repairnator/repairnator
+git checkout create-launcher-utils
 mvn clean install -DskipTests=true
 
 echo "Repairnator compiled and installed"
