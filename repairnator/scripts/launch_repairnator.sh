f9333609807cb1a6b98fc576ff7e82541e22f5b0
Fer Madeiral <fer.madeiral@gmail.com>
2018-03-14 00:11:41 -0300

    Show in the command line all args being passed in command java -jar

--- repairnator/scripts/launch_repairnator.sh
+++ repairnator/scripts/launch_repairnator.sh
@@ -69,14 +69,17 @@ cp $REPAIRNATOR_DOCKERPOOL_JAR $REPAIRNATOR_DOCKERPOOL_DEST_JAR
 if [ "$SKIP_SCAN" -eq 0 ]; then
     echo "Start to scan projects for builds (dest file: $REPAIRNATOR_BUILD_LIST)..."
 
-    args="`ca --googleSecretPath $GOOGLE_SECRET_PATH``ca --spreadsheet $SPREADSHEET``ca --dbhost $MONGODB_HOST``ca --dbname $MONGODB_NAME``ca -l $SCANNER_NB_HOURS``ca --lookFromDate $LOOK_FROM_DATE` `ca --lookToDate $LOOK_TO_DATE``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
+    elementaryArgs="-m $REPAIR_MODE -i $REPAIR_PROJECT_LIST_PATH -o $REPAIRNATOR_BUILD_LIST --runId $RUN_ID"
+
+    supplementaryArgs="`ca --googleSecretPath $GOOGLE_SECRET_PATH``ca --spreadsheet $SPREADSHEET``ca --dbhost $MONGODB_HOST``ca --dbname $MONGODB_NAME``ca -l $SCANNER_NB_HOURS``ca --lookFromDate $LOOK_FROM_DATE` `ca --lookToDate $LOOK_TO_DATE``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
 
     if [ "$NOTIFY_ENDPROCESS" -eq 1 ]; then
-        args="$args --notifyEndProcess"
+        supplementaryArgs="$supplementaryArgs --notifyEndProcess"
     fi
 
-    echo "Supplementary args for scanner: $args"
-    java -jar $REPAIRNATOR_SCANNER_DEST_JAR -m $REPAIR_MODE -i $REPAIR_PROJECT_LIST_PATH -o $REPAIRNATOR_BUILD_LIST --runId $RUN_ID -d $args &> $LOG_DIR/scanner.log
+    echo "Elementary args for scanner: $elementaryArgs"
+    echo "Supplementary args for scanner: $supplementaryArgs"
+    java -jar $REPAIRNATOR_SCANNER_DEST_JAR -d $elementaryArgs $supplementaryArgs &> $LOG_DIR/scanner.log
 fi
 
 NB_LINES=`wc -l < $REPAIRNATOR_BUILD_LIST`
@@ -97,17 +100,21 @@ echo "Pull the docker machine (name: $DOCKER_TAG)..."
 docker pull $DOCKER_TAG
 
 echo "Launch docker pool..."
-args="`ca --googleSecretPath $GOOGLE_SECRET_PATH``ca --spreadsheet $SPREADSHEET``ca --dbhost $MONGODB_HOST``ca --dbname $MONGODB_NAME``ca --pushurl $PUSH_URL``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
+
+elementaryArgs="-t $NB_THREADS -n $DOCKER_TAG -i $REPAIRNATOR_BUILD_LIST -o $LOG_DIR -l $DOCKER_LOG_DIR -g $DAY_TIMEOUT --runId $RUN_ID -m $REPAIR_MODE"
+
+supplementaryArgs="`ca --googleSecretPath $GOOGLE_SECRET_PATH``ca --spreadsheet $SPREADSHEET``ca --dbhost $MONGODB_HOST``ca --dbname $MONGODB_NAME``ca --pushurl $PUSH_URL``ca --smtpServer $SMTP_SERVER``ca --notifyto $NOTIFY_TO`"
 if [ "$NOTIFY_ENDPROCESS" -eq 1 ]; then
-    args="$args --notifyEndProcess"
+    supplementaryArgs="$supplementaryArgs --notifyEndProcess"
 fi
 
 if [ "$CREATE_OUTPUT_DIR" -eq 1 ]; then
-    args="$args --createOutputDir"
+    supplementaryArgs="$supplementaryArgs --createOutputDir"
 fi
 
-echo "Supplementary args for docker pool $args"
-java -jar $REPAIRNATOR_DOCKERPOOL_DEST_JAR -t $NB_THREADS -n $DOCKER_TAG -i $REPAIRNATOR_BUILD_LIST -o $LOG_DIR -l $DOCKER_LOG_DIR -g $DAY_TIMEOUT --runId $RUN_ID -m $REPAIR_MODE $args &> $LOG_DIR/dockerpool.log
+echo "Elementary args for docker pool: $elementaryArgs"
+echo "Supplementary args for docker pool: $supplementaryArgs"
+java -jar $REPAIRNATOR_DOCKERPOOL_DEST_JAR $elementaryArgs $supplementaryArgs &> $LOG_DIR/dockerpool.log
 
 echo "Docker pool finished, delete the run directory ($REPAIRNATOR_RUN_DIR)"
 rm -rf $REPAIRNATOR_RUN_DIR
