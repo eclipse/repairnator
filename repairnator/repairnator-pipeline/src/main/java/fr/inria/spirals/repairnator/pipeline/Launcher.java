4566d6dc73f96acc0de686acccdaf9e8b4c61c44
Simon Urli <simon.urli@gmail.com>
2018-03-20 12:28:02 +0100

    Add a new serializer to store pipeline errors (#464)

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/Launcher.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/Launcher.java
@@ -14,6 +14,7 @@ import fr.inria.spirals.repairnator.notifier.ErrorNotifier;
 import fr.inria.spirals.repairnator.serializer.AstorSerializer;
 import fr.inria.spirals.repairnator.serializer.MetricsSerializer;
 import fr.inria.spirals.repairnator.serializer.NPEFixSerializer;
+import fr.inria.spirals.repairnator.serializer.PipelineErrorSerializer;
 import fr.inria.spirals.repairnator.states.LauncherMode;
 import fr.inria.spirals.repairnator.states.ScannedBuildStatus;
 import fr.inria.spirals.repairnator.Utils;
@@ -302,6 +303,7 @@ public class Launcher {
         serializers.add(new NPEFixSerializer(this.engines));
         serializers.add(new AstorSerializer(this.engines));
         serializers.add(new MetricsSerializer(this.engines));
+        serializers.add(new PipelineErrorSerializer(this.engines));
 
         ProjectInspector inspector;
 
