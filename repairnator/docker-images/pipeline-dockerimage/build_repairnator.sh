75fbc9adc56caf1715a02af4b4e6b61b08e9ff0d
Simon Urli <simon.urli@inria.fr>
2018-04-06 15:57:26 +0200

    Use latest version (compiled from github) of Repairnator in docker images

--- repairnator/docker-images/pipeline-dockerimage/build_repairnator.sh
+++ repairnator/docker-images/pipeline-dockerimage/build_repairnator.sh
@@ -2,6 +2,19 @@
 
 set -e
 
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=fr.inria.repairnator:repairnator-pipeline:LATEST:jar:jar-with-dependencies -Ddest=/root/repairnator-pipeline.jar
+cd /root
+mkdir github
+cd github
+git clone --recursive --depth=1 https://github.com/Spirals-Team/repairnator.git
+
+echo "LibRepair repository cloned."
+
+cd repairnator/repairnator
+mvn clean install -DskipTests=true
+
+echo "Repairnator compiled and installed"
+
+cp repairnator-pipeline/target/repairnator-pipeline-*-with-dependencies.jar /root/repairnator-pipeline.jar
+#mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=fr.inria.repairnator:repairnator-pipeline:LATEST:jar:jar-with-dependencies -Ddest=/root/repairnator-pipeline.jar
 
 echo "Repairnator-pipeline jar file installed in /root directory"
