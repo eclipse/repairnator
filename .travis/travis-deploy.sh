3f547f3691b6e02996bfcb8626ae50ddb76a6acc
Simon Urli <simon.urli@inria.fr>
2018-04-11 11:29:13 +0200

    Trigger docker container build AFTER deployment

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-    cp .travis/travis-settings.xml $HOME/.m2/settings.xml && cd repairnator && mvn deploy -DskipTests
+    cp .travis/travis-settings.xml $HOME/.m2/settings.xml
+    cd repairnator
+    mvn deploy -DskipTests
+    curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/surli/repairnator/trigger/${env.DOCKER_HUB_TOKEN} # Trigger docker container build
 else
     echo "Nothing to deploy when on PR or other branch"
 fi
