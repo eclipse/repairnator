f46a38cc07354359f087c4755430990a5647e3e1
Simon Urli <simon.urli@inria.fr>
2018-04-11 12:27:55 +0200

    Fix env variable

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     cp .travis/travis-settings.xml $HOME/.m2/settings.xml
     cd repairnator
     mvn deploy -DskipTests
-    curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/surli/repairnator/trigger/${env.DOCKER_HUB_TOKEN} # Trigger docker container build
+    curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/surli/repairnator/trigger/$DOCKER_HUB_TOKEN # Trigger docker container build
 else
     echo "Nothing to deploy when on PR or other branch"
 fi
