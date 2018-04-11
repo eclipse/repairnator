444d03c707c4e5d987f5c422f8f17d8d46de0d6e
Simon Urli <simon.urli@inria.fr>
2018-04-11 10:36:00 +0200

    Only deploy when on master branch

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     cp .travis/travis-settings.xml $HOME/.m2/settings.xml && cd repairnator && mvn deploy -DskipTests
 else
-    echo "Nothing to deploy when on PR"
+    echo "Nothing to deploy when on PR or other branch"
 fi
