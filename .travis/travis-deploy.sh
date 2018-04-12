908bec9387e236d4447d03a96c3e29c775bd7114
Simon Urli <simon.urli@inria.fr>
2018-04-12 11:22:41 +0200

    Add quiet flag for docker build

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -12,7 +12,13 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
         TAG="latest"
     fi
 
-    docker build -t surli/repairnator:$TAG docker-images/pipeline-dockerimage
+    docker build -q -t surli/repairnator:$TAG docker-images/pipeline-dockerimage
+    if [[ $? != 0 ]]
+    then
+        echo "Error while building docker image"
+        exit 1
+    fi
+
     docker push surli/repairnator:$TAG
     if [[ $? != 0 ]]
     then
