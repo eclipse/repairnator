4ab285549b33301ce09353cc0d2cf7f25c803d21
Simon Urli <simon.urli@inria.fr>
2018-04-12 10:24:48 +0200

    Fix path for docker build

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
         TAG="latest"
     fi
 
-    docker build -f docker-images/pipeline-dockerimage -t surli/repairnator:$TAG
+    docker build -t surli/repairnator:$TAG docker-images/pipeline-dockerimage
     docker push surli/repairnator:$TAG
 else
     echo "Nothing to deploy when on PR or other branch"
