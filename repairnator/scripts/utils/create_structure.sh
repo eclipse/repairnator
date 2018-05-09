72c94f65525a67c4bda5d3018d92b3b0ad3b8741
Fer Madeiral <fer.madeiral@gmail.com>
2018-05-09 08:18:12 -0300

    Change the output and log folder names

--- repairnator/scripts/utils/create_structure.sh
+++ repairnator/scripts/utils/create_structure.sh
@@ -11,7 +11,12 @@ mkdir -p $ROOT_LOG_DIR
 mkdir -p $ROOT_BIN_DIR
 mkdir -p $ROOT_OUT_DIR
 
-echo "Create output dir : $REPAIR_OUTPUT_PATH"
+if [ ! -z "$RUN_ID" ]; then
+    export REPAIR_OUTPUT_PATH=$REPAIR_OUTPUT_PATH"_$RUN_ID"
+    export LOG_DIR=$LOG_DIR"_$RUN_ID"
+fi
+
+echo "Create output dir: $REPAIR_OUTPUT_PATH"
 mkdir $REPAIR_OUTPUT_PATH
 
 echo "Create log directory: $LOG_DIR"
