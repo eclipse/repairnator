c663fae3992e14940e3493be5f568bbdbbe0304d
Simon Urli <simon.urli@gmail.com>
2018-06-08 21:21:43 +0200

    review Fix maven model resolver (#598)
    This PR aims at fixing #597

---
+++ repairnator/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/process/maven/TestMavenHelper.java
@@ -0,0 +1,24 @@
+package fr.inria.spirals.repairnator.process.maven;
+
+import com.google.common.io.Files;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.ModelBuildingException;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestMavenHelper {
+	@Test
+	public void testGetMavenModel() throws ModelBuildingException {
+		File javaParserPom = new File("./src/test/resources/pom-examples/javaparser-pom.xml");
+		File tempM2 = Files.createTempDir();
+
+		Model model = MavenHelper.readPomXml(javaParserPom, tempM2.getAbsolutePath());
+		assertNotNull(model);
+
+		assertEquals(8, model.getModules().size());
+	}
+}
