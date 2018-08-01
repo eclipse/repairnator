7cf468f3ca2003b954cead3d6c4eab695ad1146c
Simon Urli <simon.urli@inria.fr>
2018-08-01 11:16:27 +0200

    Remove references also in scripts

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-
-git config --global user.name "Luc Esape"
-git config --global user.email "luc.esape@gmail.com"
-
 export M2_HOME=/usr/local/maven
 
 cd repairnator
