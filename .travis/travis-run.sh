cdb500d5e0005978f763862b438f1767fcc29989
Simon Urli <simon.urli@gmail.com>
2018-08-01 10:36:34 +0200

    Write version ini on travis only (#680)
    fix #658

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -8,6 +8,6 @@ git config --global user.email "luc.esape@gmail.com"
 export M2_HOME=/usr/local/maven
 
 cd repairnator
-mvn clean install
+mvn clean install -Ptravis
 mvn jacoco:report coveralls:report --fail-never
 
