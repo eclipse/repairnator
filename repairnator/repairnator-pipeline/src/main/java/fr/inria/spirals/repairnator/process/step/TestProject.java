d7a6d6713b2f6f3b8756fb43722df50736ef3288
Simon Urli <simon.urli@inria.fr>
2018-08-03 10:48:42 +0200

    Get rid of useless class MavenFilterTestOutputHandler

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/TestProject.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/TestProject.java
@@ -3,7 +3,7 @@ package fr.inria.spirals.repairnator.process.step;
 import fr.inria.spirals.repairnator.process.inspectors.StepStatus;
 import fr.inria.spirals.repairnator.process.inspectors.ProjectInspector;
 import fr.inria.spirals.repairnator.process.maven.MavenHelper;
-import fr.inria.spirals.repairnator.process.maven.output.MavenFilterTestOutputHandler;
+import fr.inria.spirals.repairnator.process.maven.output.MavenFilterOutputHandler;
 import fr.inria.spirals.repairnator.states.PipelineState;
 
 /**
@@ -24,8 +24,8 @@ public class TestProject extends AbstractStep {
 
         MavenHelper helper = new MavenHelper(this.getPom(), "test", null, this.getClass().getSimpleName(), this.getInspector(), false);
 
-        MavenFilterTestOutputHandler outputTestFilter = new MavenFilterTestOutputHandler(helper);
-        helper.setOutputHandler(outputTestFilter);
+        MavenFilterOutputHandler outputFilter = new MavenFilterOutputHandler(helper);
+        helper.setOutputHandler(outputFilter);
 
         try {
             helper.run();
