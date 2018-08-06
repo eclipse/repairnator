93edc7ca532dd6b9a81ba6fd14ca52db33cfd4cb
Fer Madeiral <fer.madeiral@gmail.com>
2018-08-06 10:28:24 -0300

    Try to fix another issue

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -4,6 +4,8 @@ set -e
 export M2_HOME=/usr/local/maven
 
 cd repairnator
+
+./docker-images/bears-checkbranches-dockerimage/check_branches_test.sh
+
 mvn clean install -Ptravis
 mvn jacoco:report coveralls:report --fail-never
-
