b94059e504970b7cbdd2584ad531b5c2527455b7
Fer Madeiral <fer.madeiral@gmail.com>
2018-09-10 10:53:36 -0300

    Improve check_branches.sh for Bears

--- repairnator/docker-images/bears-checkbranches-dockerimage/check_branches.sh
+++ repairnator/docker-images/bears-checkbranches-dockerimage/check_branches.sh
@@ -5,6 +5,17 @@ if [ "$#" -ne 2 ]; then
     exit 2
 fi
 
+function checkCommit {
+    echo "$1"
+    if [[ "$1" =~ "$2" ]]; then
+        echo "The commit is OK."
+    else
+        RESULT="$BRANCH_NAME [FAILURE] (the commits are not in the correct sequence or some commit is missing)"
+        >&2 echo -e "$RED $RESULT $NC"
+        echo "$RESULT" >> $DOCKER_DEST
+        exit 1
+    fi
+}
 
 DOCKER_DEST=/tmp/result.txt
 REPO=$1
@@ -18,10 +29,11 @@ command -v ajv >/dev/null 2>&1 || { echo >&2 "I require ajv (https://github.com/
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 JSON_SCHEMA="$SCRIPT_DIR/bears-schema.json"
-echo "$JSON_SCHEMA"
 if [ ! -f $JSON_SCHEMA ]; then
     echo "The json schema ($JSON_SCHEMA) cannot be found."
-    exit -1
+    exit 2
+else
+    echo "JSON schema path: $JSON_SCHEMA"
 fi
 
 MAVEN_TEST_ARGS="-Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true"
@@ -39,47 +51,82 @@ if [ -e "bears.json" ]; then
         RESULT="$BRANCH_NAME [FAILURE] (bears.json is invalid)"
         >&2 echo -e "$RED $RESULT $NC"
         echo "$RESULT" >> $DOCKER_DEST
-        exit 2
+        exit 1
     fi
 else
     RESULT="$BRANCH_NAME [FAILURE] (bears.json does not exist)"
     >&2 echo -e "$RED $RESULT $NC"
     echo "$RESULT" >> $DOCKER_DEST
-    exit 2
+    exit 1
 fi
 
 numberOfCommits=`git rev-list --count HEAD`
-
-if [ "$numberOfCommits" -ne 3 ] && [ "$numberOfCommits" -ne 4 ]; then
-    RESULT="$BRANCH_NAME [FAILURE] (the number of commits is different than 3 and 4)"
+if [ "$numberOfCommits" -lt 4 ] ; then
+    RESULT="$BRANCH_NAME [FAILURE] (the number of commits is less than 4)"
     >&2 echo -e "$RED $RESULT $NC"
     echo "$RESULT" >> $DOCKER_DEST
-    exit 2
+    exit 1
 fi
 
-bugCommitId=`git log --format=format:%H --grep="Bug commit"`
-patchCommitId=`git log --format=format:%H --grep="Human patch"`
+bugCommitId=""
 
-if [ "$numberOfCommits" -eq 4 ]; then
+case=$(cat bears.json | sed 's/.*"type": "\(.*\)".*/\1/;t;d')
+echo "Branch from case $case"
+if [ "$case" == "failing_passing" ]; then
+    echo "3 commits must exist."
+
+    echo "Checking commits..."
+
+    firstCommitMessage=`git log --format=format:%B --skip 2 -n 1`
+    checkCommit "$firstCommitMessage" "Bug commit"
+
+    secondCommitMessage=`git log --format=format:%B --skip 1 -n 1`
+    checkCommit "$secondCommitMessage" "Human patch"
+
+    thirdCommitMessage=`git log --format=format:%B -n 1`
+    checkCommit "$thirdCommitMessage" "End of the bug and patch reproduction process"
+
+    bugCommitId=`git log --format=format:%H --grep="Bug commit"`
+else
+    echo "4 commits must exist."
+
+    echo "Checking commits..."
+
+    firstCommitMessage=`git log --format=format:%B --skip 3 -n 1`
+    checkCommit "$firstCommitMessage" "Bug commit"
+
+    secondCommitMessage=`git log --format=format:%B --skip 2 -n 1`
+    checkCommit "$secondCommitMessage" "Changes in the tests"
+
+    thirdCommitMessage=`git log --format=format:%B --skip 1 -n 1`
+    checkCommit "$thirdCommitMessage" "Human patch"
+
+    fourthCommitMessage=`git log --format=format:%B -n 1`
+    checkCommit "$fourthCommitMessage" "End of the bug and patch reproduction process"
+
     bugCommitId=`git log --format=format:%H --grep="Changes in the tests"`
 fi
+
+patchCommitId=`git log --format=format:%H --grep="Human patch"`
 
 echo "Checking out the bug commit: $bugCommitId"
 git log --format=%B -n 1 $bugCommitId
 
 git checkout -q $bugCommitId
 
-timeout 1800s mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
+timeout 3000s mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
 
 status=$?
 if [ "$status" -eq 0 ]; then
-    >&2 echo -e "$RED Error while reproducing the bug for branch $BRANCH_NAME $NC (status = $status)"
-    echo "$BRANCH_NAME [FAILURE] (bug reproduction - status = $status)" >> $DOCKER_DEST
-    exit 2
+    RESULT="$BRANCH_NAME [FAILURE] (bug reproduction - status = $status)"
+    >&2 echo -e "$RED $RESULT $NC"
+    echo "$RESULT" >> $DOCKER_DEST
+    exit 1
 elif [ "$status" -eq 124 ]; then
-    >&2 echo -e "$RED Error while reproducing the bug for branch $BRANCH_NAME $NC"
-    echo "$BRANCH_NAME [FAILURE] (bug reproduction timeout)" >> $DOCKER_DEST
-    exit 2
+    RESULT="$BRANCH_NAME [FAILURE] (bug reproduction timeout)"
+    >&2 echo -e "$RED $RESULT $NC"
+    echo "$RESULT" >> $DOCKER_DEST
+    exit 1
 fi
 
 echo "Checking out the patch commit: $patchCommitId"
@@ -87,17 +134,19 @@ git log --format=%B -n 1 $patchCommitId
 
 git checkout -q $patchCommitId
 
-timeout 1800s mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
+timeout 3000s mvn -q -B test -Dsurefire.printSummary=false $MAVEN_TEST_ARGS
 
 status=$?
-if [ "$status" -eq 124 ]; then
-    >&2 echo -e "$RED Error while reproducing the passing build for branch $BRANCH_NAME $NC"
-    echo "$BRANCH_NAME [FAILURE] (patch reproduction timeout)" >> $DOCKER_DEST
-    exit 2
-elif [ "$status" -ne 0 ]; then
-    >&2 echo -e "$RED Error while reproducing the passing build for branch $BRANCH_NAME $NC (status = $status)"
-    echo "$BRANCH_NAME [FAILURE] (patch reproduction - status = $status)" >> $DOCKER_DEST
-    exit 2
+if [ "$status" -eq 0 ]; then
+    RESULT="$BRANCH_NAME [FAILURE] (patch reproduction - status = $status)"
+    >&2 echo -e "$RED $RESULT $NC"
+    echo "$RESULT" >> $DOCKER_DEST
+    exit 1
+elif [ "$status" -eq 124 ]; then
+    RESULT="$BRANCH_NAME [FAILURE] (patch reproduction timeout)"
+    >&2 echo -e "$RED $RESULT $NC"
+    echo "$RESULT" >> $DOCKER_DEST
+    exit 1
 fi
 
 RESULT="$BRANCH_NAME [OK]"
