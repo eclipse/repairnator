427f9fbc77d1d69284cb0662e4b33a818e6a2bee
Benjamin Loriot <benjamin.loriot@etu.utc.fr>
2018-10-16 13:01:33 +0200

    Removed getByStatus function

--- website/repairnator-mongo-rest-api/server/controllers/inspector.controller.js
+++ website/repairnator-mongo-rest-api/server/controllers/inspector.controller.js
@@ -29,14 +29,6 @@ function list(req, res, next) {
     .catch(e => next(e));
 }
 
-function getByStatus(req, res, next) {
-  const { limit = 50, skip = 0 } = req.query;
-  const { status } = req.params;
-  Inspector.getByStatus(status, { limit, skip })
-    .then(users => res.json(users))
-    .catch(e => next(e));
-}
-
 function count(req, res, next) {
   Inspector.count()
     .then(result => res.json(result))
