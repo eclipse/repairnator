0b42ff7df98521033a25dc5ae14b84a32d9bfeaf
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-29 09:49:27 -0300

    Move things related to dates from Serializer into DateUtils

--- repairnator/repairnator-core/src/main/java/fr/inria/spirals/repairnator/utils/DateUtils.java
+++ repairnator/repairnator-core/src/main/java/fr/inria/spirals/repairnator/utils/DateUtils.java
@@ -8,6 +8,9 @@ import java.util.Date;
 
 public class DateUtils {
 
+    private static final String MONGO_UTC_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    private static final SimpleDateFormat MONGO_DATE_FORMAT = new SimpleDateFormat(MONGO_UTC_FORMAT);
+
     private static final SimpleDateFormat tsvCompleteDateFormat = new SimpleDateFormat("dd/MM/YY HH:mm");
     private static final SimpleDateFormat csvOnlyDayFormat = new SimpleDateFormat("dd/MM/YYYY");
     private static final SimpleDateFormat fileDateFormat = new SimpleDateFormat("YYYY-MM-dd_HHmm");
@@ -22,6 +25,10 @@ public class DateUtils {
 
     public static String formatFilenameDate(Date date) {
         return fileDateFormat.format(date);
+    }
+
+    public static String formatDateForMongo(Date date) {
+        return MONGO_DATE_FORMAT.format(date);
     }
 
     public static String getDuration(Date dateBegin, Date dateEnd) {
