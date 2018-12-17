488e3a8a7fdca88701ead49b23cd0594cc789642
btellstrom <benjamin.tellstrom@gmail.com>
2018-12-17 15:37:36 +0100

    further work

--- repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/notifier/TimedSummaryNotifier.java
+++ repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/notifier/TimedSummaryNotifier.java
@@ -1,5 +1,6 @@
 package fr.inria.spirals.repairnator.realtime.notifier;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,17 +9,27 @@ import java.util.Iterator;
 import java.util.List;
 
 import fr.inria.spirals.repairnator.notifier.engines.NotifierEngine;
+
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import org.bson.Document; 
+import org.bson.Document;
+import org.bson.conversions.Bson; 
 
 public class TimedSummaryNotifier implements Runnable{
+    
+    private static final String MONGO_UTC_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    private static final SimpleDateFormat MONGO_DATE_FORMAT = new SimpleDateFormat(MONGO_UTC_FORMAT);
+    private static final long TIME_TO_SLEEP = 3600 * 1000;
+    private static final String[] COLLECTIONS_TO_QUERY = {"patches", 
+                                                         "RTScanner"
+                                                         };
+    
     private List<NotifierEngine> engines;
     private MongoDatabase mongo;
     private Calendar lastNotificationTime;
     private Duration interval;
-    private 
     
     
     
@@ -65,22 +76,36 @@ public class TimedSummaryNotifier implements Runnable{
     public void run() {
         while(true) {
             if(Duration.between(lastNotificationTime.getTime().toInstant(), (new Date()).toInstant()).compareTo(interval) < 0) {
-                
+                createBsonFilter();
                 String message = "Summary email from Repairnator. \n\n";
-                message += "The last summary was sent on " + lastNotificationTime.getTime().toString() " and it is now ";
+                message += "This summary contains the operations of Repairnator between " + lastNotificationTime.getTime().toString();
                 updateCalendar();
+                message += " and ";
                 message += lastNotificationTime.getTime().toString() + ".\n";
                 message += "Since the last summary Repairnator has: \n";
                 
             }
+            else {
+                try {
+                    Thread.sleep(TIME_TO_SLEEP);
+                } catch (InterruptedException e) {
+                    
+                }
+            }
         }
     }
     
-    private Iterator<Document> queryDatabase(MongoCollection<Document> mongoCollection){
-        
+    private Iterator<Document> queryDatabase(MongoCollection<Document> mongoCollection, Bson filter){
+        FindIterable<Document> iterDoc = mongoCollection.find(filter);
+        return iterDoc.iterator();
     }
     
-    private void createBsonFilter() {
+    private Bson getBsonFilter() {
+        Date previousDate = lastNotificationTime.getTime();
+        return new Document("$gte", MONGO_DATE_FORMAT.format(previousDate));
+    }
+    private Bson getBsonFilter(String toolName) {
+        Date previousDate = lastNotificationTime.getTime();
         
     }
 }
