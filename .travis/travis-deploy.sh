c1856bd99853752cc010f9b190b8e537b9de5708
Martin Monperrus <monperrus@users.noreply.github.com>
2019-02-19 13:43:12 +0100

    Update travis-deploy.sh

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -4,6 +4,8 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     cp .travis/travis-settings.xml $HOME/.m2/settings.xml
     cd repairnator
+    
+    # pushing snapshot to https://oss.sonatype.org/content/repositories/snapshots/fr/inria/repairnator/
     mvn deploy -DskipTests
     echo "$DOCKER_PASSWORD" | docker login -u repairnator --password-stdin
     if [ "$TRAVIS_TAG" = "$TRAVIS_BRANCH" ]; then
@@ -19,6 +21,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
         exit 1
     fi
 
+    # pushing to dockerhub https://hub.docker.com/r/spirals/repairnator
     docker push spirals/repairnator:$TAG
     if [[ $? != 0 ]]
     then
@@ -26,22 +29,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
         exit 1
     else
         echo "Docker image pushed: spirals/repairnator:$TAG"
-    fi
-
-    docker build -q -t spirals/bears:$TAG docker-images/bears-dockerimage
-    if [[ $? != 0 ]]
-    then
-        echo "Error while building bears docker image"
-        exit 1
-    fi
-
-    docker push spirals/bears:$TAG
-    if [[ $? != 0 ]]
-    then
-        echo "Error while pushing bears docker image"
-        exit 1
-    else
-        echo "Docker image pushed: spirals/bears:$TAG"
     fi
 else
     echo "Nothing to deploy when on PR or other branch"
