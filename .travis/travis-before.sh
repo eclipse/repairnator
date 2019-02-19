4cc80057eba641aaf91ec5c083e3ae2988a72ae6
Benjamin Tellström <36193562+btellstrom@users.noreply.github.com>
2019-02-19 14:48:58 +0100

    Deploy travis in stages to enable auto-deployment of docker-image (#809)
    * WIP to make travis work with stages
    
    * Makes changes to the build configuration of travis
    
    * added travis-maven-repair file
    
    * Added matrix to .travis.yml
    
    * Attempt to circumvent the fact that matrix does not work with stages
    
    * Update travis-deploy.sh
    
    * Typo

---
+++ .travis/travis-before.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install cloc -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g ajv-cli
+sudo apt-get install shunit2
