eb262fe1e3ce7252d9aa49c7d376745b19986e82
Martin Monperrus <monperrus@users.noreply.github.com>
2019-07-19 17:29:52 +0200

    chore: continuous deployment to new DockerHub org repairnator (#872)

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
+
+### MAVEN CENTRAL
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     cp .travis/travis-settings.xml $HOME/.m2/settings.xml
     cd repairnator
     
     # pushing snapshot to https://oss.sonatype.org/content/repositories/snapshots/fr/inria/repairnator/
     mvn deploy -DskipTests
+fi
 
-    exit
 
+### DOCKERHUB
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     # deploy to Dockerhub
-    echo "$DOCKER_PASSWORD" | docker login -u repairnator --password-stdin
+
+    # $DOCKER_PASSWORD is set in the Travis UI
+    echo "$DOCKER_PASSWORD" | docker login -u monperrus --password-stdin
     if [ "$TRAVIS_TAG" = "$TRAVIS_BRANCH" ]; then
         TAG=$TRAVIS_TAG
     else
         TAG="latest"
     fi
 
-    docker build -q -t spirals/repairnator:$TAG docker-images/pipeline-dockerimage
+    # building the image
+    docker build -q -t repairnator/pipeline:$TAG docker-images/pipeline-dockerimage
     if [[ $? != 0 ]]
     then
         echo "Error while building pipeline docker image"
         exit 1
     fi
-
-    # pushing to dockerhub https://hub.docker.com/r/spirals/repairnator
-    docker push spirals/repairnator:$TAG
+    # pushing to dockerhub 
+    docker push repairnator/pipeline:$TAG
     if [[ $? != 0 ]]
     then
         echo "Error while pushing pipeline docker image"
@@ -34,6 +40,8 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     else
         echo "Docker image pushed: spirals/repairnator:$TAG"
     fi
-else
-    echo "Nothing to deploy when on PR or other branch"
 fi
+
+
+
+
