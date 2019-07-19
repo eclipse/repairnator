eff9496db4323d5037530e786d4e48004fc0235c
tailp <tailp@kth.se>
2019-07-19 22:52:48 +0200

    Feat: creating a new class ActiveMQPipelineRunner

---
+++ repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/ActiveMQPipelineRunner.java
@@ -0,0 +1,11 @@
+package fr.inria.spirals.repairnator.realtime;
+
+import fr.inria.spirals.repairnator.dockerpool.AbstractPoolManager;
+
+public class ActiveMQPipelineRunner extends AbstractPoolManager {
+
+
+    public ActiveMQPipelineRunner(RTScanner rtScanner) {
+    }
+
+}
