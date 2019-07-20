73af73d83a376ec36f09b288ca1b6c6a2754f9e6
tailp <tailp@kth.se>
2019-07-20 13:37:33 +0200

    Fix: add comment about future implementation of ActiveMQPipelineRunner

--- repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/ActiveMQPipelineRunner.java
+++ repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/ActiveMQPipelineRunner.java
@@ -2,6 +2,11 @@ package fr.inria.spirals.repairnator.realtime;
 
 import fr.inria.spirals.repairnator.dockerpool.AbstractPoolManager;
 
+/**
+ * This class will take the builds qualified by the inspectBuild
+ * and submit to an ActiveMQ queue for the repairnator-worker
+ * to run on Kubernetes.
+ */
 public class ActiveMQPipelineRunner extends AbstractPoolManager {
 
 
