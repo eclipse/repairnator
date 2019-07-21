b97ba897f38dddb0599e1b94faab9d2ba55e43b8
tailp <tailp@kth.se>
2019-07-21 13:54:24 +0200

    Fix: revert changes pushed to wrong branch

--- repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/PipelineRunner.java
+++
@@ -1,9 +0,0 @@
-package fr.inria.spirals.repairnator.realtime;
-
-/**
- * This interface describes the core behaviour
- * PipelineRunner Classes without using Docker.
- */
-public interface PipelineRunner <T,C>{
-    T submitBuild(C c);
-}
