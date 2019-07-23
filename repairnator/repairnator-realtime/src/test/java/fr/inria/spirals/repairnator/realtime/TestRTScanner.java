5dfdc3daf2ab8096bc3a8d2cd7e9aaa3248690af
tailp <tailp@kth.se>
2019-07-23 23:35:00 +0200

    Feat: Add testcase for ActiveMQPipelineRunner, change travis to deploy activeMQ

--- repairnator/repairnator-realtime/src/test/java/fr/inria/spirals/repairnator/realtime/TestRTScanner.java
+++ repairnator/repairnator-realtime/src/test/java/fr/inria/spirals/repairnator/realtime/TestRTScanner.java
@@ -74,4 +74,15 @@ public class TestRTScanner {
         boolean result = rtScanner.isRepositoryInteresting(repositoryOptional.get().getId());
         assertFalse(result);
     }
+
+    /**
+     * Note this test might fail locally if you don't have activeMQ
+     * In that case this test can be temporarily be commented out
+     */
+    @Test
+    public void testActiveMQRunnerConnection()
+    {
+        ActiveMQPipelineRunner activeRunner = new ActiveMQPipelineRunner();
+        assertTrue(activeRunner.testConnection());
+    }
 }
