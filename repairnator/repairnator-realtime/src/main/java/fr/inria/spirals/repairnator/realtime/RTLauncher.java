d4de176a1a2155843e313c2d52ffbd876cc88cbe
tailp <tailp@kth.se>
2019-07-24 15:03:34 +0200

    Feat: remove unused line

--- repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/RTLauncher.java
+++ repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/RTLauncher.java
@@ -33,7 +33,7 @@ public class RTLauncher {
     private RepairnatorConfig config;
     private EndProcessNotifier endProcessNotifier;
     private TimedSummaryNotifier summaryNotifier;
-    private boolean kubernetesmode;
+
     private String activeMQUrl;
     private String activeMQQueueName;
 
