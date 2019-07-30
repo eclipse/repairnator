7d8c58dabfbbf9e7a1a6b005805f371a43710b60
Martin Monperrus <martin.monperrus@gnieh.org>
2019-07-30 10:27:14 +0200

    Commit by Martin Monperrus on 30 July 2019

---
+++ repairnator/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/pipeline/TestPipeline.java
@@ -0,0 +1,37 @@
+package fr.inria.spirals.repairnator.pipeline;
+
+import ch.qos.logback.classic.Level;
+import fr.inria.spirals.repairnator.config.RepairnatorConfig;
+import fr.inria.spirals.repairnator.pipeline.Launcher;
+import fr.inria.spirals.repairnator.process.inspectors.JobStatus;
+import fr.inria.spirals.repairnator.process.inspectors.ProjectInspector;
+import fr.inria.spirals.repairnator.process.utils4tests.ProjectInspectorMocker;
+import fr.inria.spirals.repairnator.utils.Utils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by urli on 21/02/2017.
+ */
+public class TestPipeline {
+
+    @Test
+    public void testPipeline() throws Exception {
+        // requires env variable M2_HOME and GITHUB_OAUTH
+        // (set in Travis config)
+        // from surli/failingBuild
+        Launcher l = new Launcher(new String[]{"--build", "564711868"});
+        l.mainProcess();
+
+    }
+}
