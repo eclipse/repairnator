ec2c9d4d32a003afdaba4c81d0e7aa9119ff6cff
tailp <tailp@kth.se>
2019-07-31 11:32:53 +0200

    Fix: remove MessageListener implementation

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/PipelineBuildListener.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/PipelineBuildListener.java
@@ -1,8 +1,6 @@
 package fr.inria.spirals.repairnator.pipeline;
 
-import javax.jms.MessageListener;
-
 /**
  * This class listen for build ids from ActiveMQ queue and run the pipeline with it.
  */
-public class PipelineBuildListener implements MessageListener {}
+public class PipelineBuildListener{}
