1a9836bd1f79321205bdfaf8393cc6231e8fd012
tailp <tailp@kth.se>
2019-08-02 00:45:41 +0200

    Feat: add basic structure for build-rainer

---
+++ repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
@@ -0,0 +1,6 @@
+package fr.inria.repairnator;
+
+public class BuildRainer {
+    public BuildRainer () {}
+    public void startRaining() {}
+}
---
+++ repairnator/build-rainer/src/main/java/fr/inria/repairnator/Launcher.java
@@ -0,0 +1,8 @@
+package fr.inria.repairnator;
+
+public class Launcher {
+    public static void main( String[] args ) {
+        BuildRainer buildRainer = new BuildRainer();
+        buildRainer.startRaining();
+    }
+}
