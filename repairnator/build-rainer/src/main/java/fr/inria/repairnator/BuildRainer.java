f11aded456edbf9d72db1837dd519b064585d8b3
Tailp <33935517+Tailp@users.noreply.github.com>
2019-08-02 15:45:31 +0200

    Feat: basic structure for build-rainer component (#913)

---
+++ repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
@@ -0,0 +1,9 @@
+package fr.inria.repairnator;
+
+/**
+ * This is a websocket, intended for listening to "tdurieux/travis-listener"
+ * to fetch most recent builds from Travis in realtime.
+ */
+public class BuildRainer {
+    public static void main( String[] args ) {}
+}
