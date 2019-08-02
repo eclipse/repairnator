e5c2ec2713cddb0596a8619e1f0cf8a1c50970b4
tailp <tailp@kth.se>
2019-08-02 14:17:56 +0200

    Feat: add javadoc comment

--- repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
+++ repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
@@ -1,5 +1,9 @@
 package fr.inria.repairnator;
 
+/**
+ * This is a websocket, intended for listening to "tdurieux/travis-listener"
+ * to fetch fetch most recent builds from Travis in realtime.
+ */
 public class BuildRainer {
     public static void main( String[] args ) {}
 }
