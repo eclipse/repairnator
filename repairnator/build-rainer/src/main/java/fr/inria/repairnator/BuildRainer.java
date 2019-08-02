80486801a73d9d6e57f0f2699a8baba1262c3817
tailp <tailp@kth.se>
2019-08-02 14:18:56 +0200

    Fix: remove duplicated word

--- repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
+++ repairnator/build-rainer/src/main/java/fr/inria/repairnator/BuildRainer.java
@@ -2,7 +2,7 @@ package fr.inria.repairnator;
 
 /**
  * This is a websocket, intended for listening to "tdurieux/travis-listener"
- * to fetch fetch most recent builds from Travis in realtime.
+ * to fetch most recent builds from Travis in realtime.
  */
 public class BuildRainer {
     public static void main( String[] args ) {}
