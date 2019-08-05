82b39a76f7ab2cba1489289ac7a84f6c3f9f06eb
tailp <tailp@kth.se>
2019-08-05 20:51:43 +0200

    Fix: fix error

--- repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/ScannerBuildListener.java
+++ repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/ScannerBuildListener.java
@@ -1,17 +1,19 @@
 package fr.inria.spirals.repairnator.scanner;
 
 public class ScannerBuildListener {
-	private static ScannerBuildListener scannerBuildListener;
-	public ScannerBuildListener (){}
-	public void setLauncher (Launcher launcher) {
-    	this.launcher = launcher;
+    private static ScannerBuildListener scannerBuildListener;
+    private static Launcher launcher;
+
+    public ScannerBuildListener (){}
+    public void setLauncher (Launcher launcher) {
+        this.launcher = launcher;
     }
 
     public static ScannerBuildListener getInstance() {
-    	if (scannerBuildListener == null) {
-    		scannerBuildListener = new ScannerBuildListener();
-    	}
-    	return scannerBuildListener;
+        if (scannerBuildListener == null) {
+            scannerBuildListener = new ScannerBuildListener();
+        }
+        return scannerBuildListener;
     }
     public void runAsConsumerServer() {}
 }
