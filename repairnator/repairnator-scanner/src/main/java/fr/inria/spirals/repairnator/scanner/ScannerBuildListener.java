81025d921ffbf8ea0d37f45c673a89e414780b83
tailp <tailp@kth.se>
2019-08-06 11:16:27 +0200

    Feat: implement interface

--- repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/ScannerBuildListener.java
+++ repairnator/repairnator-scanner/src/main/java/fr/inria/spirals/repairnator/scanner/ScannerBuildListener.java
@@ -1,6 +1,9 @@
 package fr.inria.spirals.repairnator.scanner;
 
-public class ScannerBuildListener {
+import fr.inria.spirals.repairnator.BuildListener;
+import javax.jms.Message;
+
+public class ScannerBuildListener implements BuildListener{
     private static ScannerBuildListener scannerBuildListener;
     private static Launcher launcher;
 
@@ -16,4 +19,13 @@ public class ScannerBuildListener {
         return scannerBuildListener;
     }
     public void runAsConsumerServer() {}
+
+
+    /**
+     * Method implemented from MessageListener and is called 
+     * each time this is done with the previous message
+     *
+     * @param message ActiveMQ message object containing a string message.
+     */
+    public void onMessage(Message message) {}
 }
