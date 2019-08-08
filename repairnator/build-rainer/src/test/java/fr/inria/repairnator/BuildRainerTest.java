2bd3637e93b953d151381b17df11949fdf06c620
tailp <tailp@kth.se>
2019-08-08 17:28:29 +0200

    Re-add tests

--- repairnator/build-rainer/src/test/java/fr/inria/repairnator/BuildRainerTest.java
+++ repairnator/build-rainer/src/test/java/fr/inria/repairnator/BuildRainerTest.java
@@ -7,4 +7,31 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BuildRainerTest {
+
+	@Ignore
+	/**
+	 * This test if BuildRainer can connect
+	 * and communicate with a ws server
+     * Remove ignore to test, this can be a bit flaky.
+	 */
+	public void testWebSocket()
+    {
+    	TestServer testServer =TestServer.getInstance();
+    	testServer.setReuseAddr(true);
+        testServer.run();
+        String receivedMsg = testServer.getBuildRainer().getRecentMessage();
+
+        assertEquals(receivedMsg,"Test");
+    }
+
+    @Test
+    /**
+     * Test if ActiveMQSubmitter can successfully submit to queue
+     */
+    public void testActiveMQSubmitter()
+    {
+        ActiveMQBuildSubmitter submitter = new ActiveMQBuildSubmitter("tcp://localhost:61616","testQueue");
+        submitter.submit("Test");
+        assertEquals(submitter.receiveFromQueue(),"Test");
+    }
 }
