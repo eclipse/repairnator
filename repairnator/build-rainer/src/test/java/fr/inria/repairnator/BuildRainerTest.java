21b3af6981d07008572eb6d862e1195d139c6f4b
Tailp <33935517+Tailp@users.noreply.github.com>
2019-08-08 10:03:14 +0200

    Feat: BuildRainer inital implementation. (#917)

--- repairnator/build-rainer/src/test/java/fr/inria/repairnator/BuildRainerTest.java
+++ repairnator/build-rainer/src/test/java/fr/inria/repairnator/BuildRainerTest.java
@@ -1,3 +1,24 @@
 package fr.inria.repairnator;
 
-public class BuildRainerTest {}
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BuildRainerTest {
+
+	@Test
+	/**
+	 * This test if BuildRainer can connect
+	 * and communicate with a ws server
+	 */
+	public void testWebSocket()
+    {
+    	TestServer testServer =TestServer.getInstance();
+    	testServer.setReuseAddr(true);
+        testServer.run();
+        String receivedMsg = testServer.getBuildRainer().getRecentMessage();
+
+        assertEquals(receivedMsg,"Test");
+    }
+}
