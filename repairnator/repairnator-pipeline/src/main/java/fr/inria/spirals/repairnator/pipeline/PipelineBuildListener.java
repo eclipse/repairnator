0cdd6dedbe283e489005c7cecd0d5339b3354eff
Tailp <33935517+Tailp@users.noreply.github.com>
2019-09-02 14:00:46 +0200

    Feat: implement activemq in pipeline (#936)

--- repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/PipelineBuildListener.java
+++ repairnator/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/pipeline/PipelineBuildListener.java
@@ -6,7 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.Message;
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.BytesMessage;
 import javax.jms.MessageListener;
+import javax.jms.MessageConsumer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 /**
  * This class fetch build ids from ActiveMQ queue and run the pipeline with it.
@@ -24,7 +33,22 @@ public class PipelineBuildListener implements Listener,MessageListener {
     /**
      * Run this as a listener server and fetch one message as a time
      */
-    public void runListenerServer() {}
+    public void runListenerServer() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(config.getActiveMQUrl() + "?jms.prefetchPolicy.all=1");
+        Connection connection;
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(false,Session.CLIENT_ACKNOWLEDGE);
+            Destination queue = session.createQueue(config.getActiveMQListenQueueName());
+
+            MessageConsumer consumer = session.createConsumer(queue);
+            consumer.setMessageListener(this);
+            LOGGER.warn("Server is now listening for build ids");
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * Method implemented from MessageListener and is called 
@@ -32,7 +56,29 @@ public class PipelineBuildListener implements Listener,MessageListener {
      *
      * @param message ActiveMQ message object containing a string buildId.
      */
-    public void onMessage(Message message) {}
+    public void onMessage(Message message) {
+        String messageText = null;
+        try {
+            message.acknowledge();
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                messageText = textMessage.getText();
+                LOGGER.info("A new buildId has arrived: " + messageText);
+                config.setBuildId(Integer.parseInt(messageText));
+                this.launcher.mainProcess();
+            } else if (message instanceof BytesMessage) {
+                BytesMessage bytesMessage = (BytesMessage) message;
+                byte[] data = new byte[(int) bytesMessage.getBodyLength()];
+                bytesMessage.readBytes(data);
+                messageText = new String(data);
+                LOGGER.info("A new buildId has arrived: " + messageText);
+                config.setBuildId(Integer.parseInt(messageText));
+                this.launcher.mainProcess();
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void submitBuild(String buildStr){}
 }
