57133cd47fb0a0daef0a2314febca2cbd067fd07
Jian GU <ycaxgjd@gmail.com>
2019-09-19 20:22:18 +0200

    Update travis-run.sh
    Re-run Maven using the -X switch to enable full debug logging.

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -6,7 +6,7 @@ export M2_HOME=/usr/local/maven
 
 cd repairnator
 
-mvn clean test -B -e
+mvn clean test -B -X
 
 # printing timing results to identify slow tests
 grep -h time= */target/surefire-reports/*xml
