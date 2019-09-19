b228d7040b8d238a377b8defacfca3249ac623b5
Jian GU <ycaxgjd@gmail.com>
2019-09-19 21:30:43 +0200

    Update travis-run.sh
    Running a Single Unit Test to help debug

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -6,7 +6,8 @@ export M2_HOME=/usr/local/maven
 
 cd repairnator
 
-mvn clean test -B -X
+# mvn clean test -B
+mvn clean test -Dtest=fr.inria.spirals.repairnator.process.step.repair.TestSequencerRepair -X -e
 
 # printing timing results to identify slow tests
 grep -h time= */target/surefire-reports/*xml
