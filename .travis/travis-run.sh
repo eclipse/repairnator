fc468e41e2057f9c1e6a8d6e0464106bc90f9602
Jian GU <ycaxgjd@gmail.com>
2019-09-21 13:39:12 +0200

    Update travis-run.sh
    restore
    
    Signed-off-by: Jian GU <ycaxgjd@gmail.com>

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -7,7 +7,6 @@ export M2_HOME=/usr/local/maven
 cd repairnator
 
 mvn clean test -B
-# mvn clean test -DfailIfNoTests=false -Dtest=fr.inria.spirals.repairnator.process.step.repair.TestSequencerRepair -Dsurefire.useFile=false -Dsurefire.trimStackTrace=false || tail -n 100 /home/travis/build/eclipse/repairnator/repairnator/repairnator-pipeline/target/surefire-reports/fr.inria.spirals.repairnator.process.step.repair.TestSequencerRepair-output.txt
 
 # printing timing results to identify slow tests
 grep -h time= */target/surefire-reports/*xml
