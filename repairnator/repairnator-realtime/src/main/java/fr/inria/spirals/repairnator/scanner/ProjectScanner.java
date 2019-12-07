412c751d02a0c4021a19b567c5007f54b4621720
Martin Monperrus <martin.monperrus@gnieh.org>
2019-12-07 16:28:52 +0100

    @monperrus at work ✊

--- repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/scanner/ProjectScanner.java
+++ repairnator/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/scanner/ProjectScanner.java
@@ -229,6 +229,9 @@ public class ProjectScanner {
         return results;
     }
 
+    /** warning: this method is slow, because method `getBeforeOrAfter()` iterates over all builds of the repo of `build`
+     * which can be very slow for big repos
+     */
     public BuildToBeInspected getBuildToBeInspected(Build build) {
         if (testBuild(build)) {
             if (RepairnatorConfig.getInstance().getLauncherMode() == LauncherMode.REPAIR) {
