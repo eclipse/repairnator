9619cbee829e614a43b8b3c42b7e495ddabafa60
Jian GU <jianguda@gmail.com>
2020-01-29 14:53:56 +0100

    fix the issue of sending duplicated messages

--- repairnator/repairnator-github-app/lib/touch-mq.js
+++ repairnator/repairnator-github-app/lib/touch-mq.js
@@ -19,21 +19,21 @@ module.exports = (buildId) => {
     });
     var sender = connection.open_sender(args.queue);
 
-    container.on('connection_open', function (context) {
+    connection.on('connection_open', function (context) {
         console.log('authenticated!');
     });
-    container.on('sendable', function (context) {
+    connection.on('sendable', function (context) {
         console.log('sent ' + buildId + '!');
         // if send plain text, when buildId = 999, then AciveMQ will get: 999
-        // context.sender.send({ body: buildId })
+        context.sender.send({ body: buildId })
         // if send raw binary, when buildId = 999, then ActiveMQ will get: {"buildId":"999"}
-        var amqp_message = container.message;
-        const stringifiedPayload = JSON.stringify({ 'buildId': buildId });
-        const buffer = new Buffer.from(stringifiedPayload, 'utf8');
-        const body = amqp_message.data_section(buffer);
-        context.sender.send({ body });
+        // var amqp_message = container.message;
+        // const stringifiedPayload = JSON.stringify({ 'buildId': buildId });
+        // const buffer = new Buffer.from(stringifiedPayload, 'utf8');
+        // const body = amqp_message.data_section(buffer);
+        // context.sender.send({ body });
     });
-    container.on('accepted', function (context) {
+    connection.on('accepted', function (context) {
         console.log('accepted!');
         context.connection.close();
         console.log('disconnected!');
