84b2d6535c66f4399cbb98b1e9ad5df1744e98d9
ginellidavide@gmail.com <ginellidavide@gmail.com>
2020-02-16 14:41:39 -0800

    Update documentation of the class LauncherMode
    Signed-off-by: ginellidavide@gmail.com <ginellidavide@gmail.com>

--- src/repairnator-core/src/main/java/fr/inria/spirals/repairnator/states/LauncherMode.java
+++ src/repairnator-core/src/main/java/fr/inria/spirals/repairnator/states/LauncherMode.java
@@ -2,26 +2,26 @@ package fr.inria.spirals.repairnator.states;
 
 /**
  * Created by urli on 25/01/2017.
- * 
+ *
  * Launcher modes that can be used to run Repairnator.
  */
 public enum LauncherMode {
-    
+
 	/**
 	 * REPAIR: This mode allows to use Repairnator to analyze bugs present in a Continuous Integration build.
 	 */
 	REPAIR,
-    
+
 	/**
 	 * BEARS: This mode allows to use Repairnator to analyze pairs of bugs and human-produced patches.
 	 */
 	BEARS,
-	
+
 	/**
 	 * CHECKSTYLE: This mode allows to use Repairnator to analyze build failing because of checkstyle.
 	 */
     CHECKSTYLE,
-    
+
     /**
      * GIT_REPOSITORY: This mode allows to use Repairnator to analyze bugs present in a Git repository.
      */
