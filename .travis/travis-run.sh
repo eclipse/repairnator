85452bfe555ddc947008a4424ae7f3c2a4fcba2b
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-19 09:04:18 +0100

    @monperrus at work

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -4,15 +4,7 @@
 set -e
 export M2_HOME=/usr/local/maven
 
-cd src
-
 mvn clean test -B
-
-# build and test jenkins plugin
-
-cd repairnator-jenkins-plugin
-
-mvn clean test
 
 cd ..
 # printing timing results to identify slow tests
