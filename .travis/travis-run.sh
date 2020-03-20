b707d3b982561938834f571d0510ea2aed4f0b2f
Martin Monperrus <monperrus@users.noreply.github.com>
2020-03-20 07:32:28 +0100

    ref: introduce top-level pom file (#1054)

--- .travis/travis-run.sh
+++ .travis/travis-run.sh
@@ -4,16 +4,5 @@
 set -e
 export M2_HOME=/usr/local/maven
 
-cd src
-
 mvn clean test -B
 
-# build and test jenkins plugin
-
-cd repairnator-jenkins-plugin
-
-mvn clean test
-
-cd ..
-# printing timing results to identify slow tests
-grep -h time= */target/surefire-reports/*xml
