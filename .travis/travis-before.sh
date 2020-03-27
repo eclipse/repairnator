0f305261f66a6ddca607b0abb4ead8bd4e6ca4a3
tailp <tailp@kth.se>
2020-03-27 14:14:43 +0100

    Feat: remove docker pulls from .travis-before

--- .travis/travis-before.sh
+++ .travis/travis-before.sh
@@ -4,8 +4,3 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g ajv-cli
 sudo apt-get install shunit2
-docker pull repairnator/pipeline
-docker pull repairnator/sequencer:1.0
-docker pull antonw/activemq-jmx:latest
-docker run -d --net=host antonw/activemq-jmx:latest
-docker ps -a
