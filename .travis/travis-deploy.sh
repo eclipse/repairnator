4fc54a2194a6def3785ea1a51a3bfb9efb2e68fc
Martin Monperrus <martin.monperrus@gnieh.org>
2020-06-21 09:12:05 +0200

    chore: fix CD

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+# CD for repairnator
+# Continuous delivery & Continuous deployment
+
+# we stop at the first failure
 set -e
 
 sudo apt-get update
 sudo apt-get install -y xmlstarlet
 
-# install the latest kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl \
-  && sudo chmod +x ./kubectl \
-  && sudo mv ./kubectl /usr/local/bin/
 
 ### MAVEN CENTRAL
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -45,22 +45,13 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "Error while pushing pipeline docker image"
         exit 1
     else
-        echo "Docker image pushed"
-
-        ### KUBERNETES
-        echo "Restart the repairnator pipelines hosted in the k8s"
-        # back to the root folder of the project
-        cd ..
-        # build the .kube directory and setup the config
-        mkdir ${HOME}/.kube
-        cp .travis/kubeconfig.skeleton ${HOME}/.kube/config
-        sed -i 's/K8S_CERTIFICATE_AUTHORITY_DATA/'"$K8S_CERTIFICATE_AUTHORITY_DATA"'/g' ${HOME}/.kube/config
-        sed -i 's/K8S_CLIENT_KEY_DATA/'"$K8S_CLIENT_KEY_DATA"'/g' ${HOME}/.kube/config
-        sed -i 's/K8S_TOKEN/'"$K8S_TOKEN"'/g' ${HOME}/.kube/config
-        # remove the current pipeline pods, k8s will automatically recreate them using the latest image
-        timeout 20s kubectl delete pod $(kubectl get pods -n repairnator -l app=repairnator-pipeline -o custom-columns=NAME:.metadata.name --no-headers=true) -n repairnator || true
+        echo "Docker image pushed to Dockerhub"
     fi
 fi
+
+## CONTINUOUS DEPLOYMENT
+# The following script udpates and restarts the known instances of Repairnator
+curl -s https://raw.githubusercontent.com/repairnator/repairnator-cd/master/repairnator-cd.sh | bash
 
 
 
