0f49fdcc01dc943c98fc9a777d276ba40758e509
André Silva <andre15andre@hotmail.com>
2020-11-05 13:16:34 +0100

    Change maven-repair CI to include repairnator-core
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- .travis/travis-maven-repair.sh
+++ .travis/travis-maven-repair.sh
@@ -2,11 +2,13 @@
 # builds the maven-repair plugin
 
 set -e
+export M2_HOME=/usr/local/maven
 
-cd src/maven-repair
+sudo apt-get update
+sudo apt-get install -y xmlstarlet
 
-NPEFIX_VERSION=`python3 -c 'import xml.etree.ElementTree; print(xml.etree.ElementTree.parse("pom.xml").findall("//mvn:dependency[mvn:artifactId=\"npefix\"]/mvn:version",{"mvn":"http://maven.apache.org/POM/4.0.0"})[0].text)'`
+VERSION=`xmlstarlet sel -t -v '//_:project/_:properties/_:revision' src/pom.xml`
+sed -i -e 's/\${revision}/'$VERSION'/' src/repairnator-core/pom.xml
+NPEFIX_VERSION=`python3 -c 'import xml.etree.ElementTree; print(xml.etree.ElementTree.parse("src/maven-repair/pom.xml").findall(".//mvn:dependency[mvn:artifactId=\"npefix\"]/mvn:version",{"mvn":"http://maven.apache.org/POM/4.0.0"})[0].text)'`
 
-mvn test -DNPEFIX_VERSION=$NPEFIX_VERSION
-
-cd ..
+mvn clean install -B -f src/repairnator-core/ && mvn -Dtest=$TEST_LIST -DNPEFIX_VERSION=$NPEFIX_VERSION clean test -B -f $TEST_PATH
