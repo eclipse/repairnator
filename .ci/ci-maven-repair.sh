4c3098b10a9ade5abc5da6c34d802678229e418c
André Silva <andre15andre@hotmail.com>
2020-12-09 12:30:36 +0000

    fix: Maven config transitions from root to child pom files (#1156)

--- .ci/ci-maven-repair.sh
+++ .ci/ci-maven-repair.sh
@@ -4,11 +4,6 @@
 set -e
 export M2_HOME=/usr/local/maven
 
-VERSION=`xmlstarlet sel -t -v '//_:project/_:properties/_:revision' src/pom.xml`
-sed -i -e 's/\${revision}/'$VERSION'/' src/repairnator-core/pom.xml
-sed -i -e 's/\${revision}/'$VERSION'/' src/pom.xml
-sed -i -e 's/\${revision}/'$VERSION'/' "${TEST_PATH}"/pom.xml
-
 NPEFIX_VERSION=`xmlstarlet sel -t -v '//_:dependency[_:artifactId="npefix"]/_:version' src/maven-repair/pom.xml`
 
 mvn clean install -B -f src/repairnator-core/ && mvn -Dtest=$TEST_LIST -DNPEFIX_VERSION=$NPEFIX_VERSION clean test -B -f $TEST_PATH -DskipTests
