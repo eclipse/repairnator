50624409698a9c072e4321735399988459a22437
André Silva <andre15andre@hotmail.com>
2021-01-15 18:53:21 +0000

    Add MAVEN_OPTS to ci-run-with-core. Increase timeout for maven-repair job.
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- .ci/ci-maven-repair.sh
+++ .ci/ci-maven-repair.sh
@@ -7,4 +7,6 @@ export M2_HOME=/usr/share/maven
 
 NPEFIX_VERSION=`xmlstarlet sel -t -v '//_:dependency[_:artifactId="npefix"]/_:version' src/maven-repair/pom.xml`
 
-mvn clean install -B -f src/repairnator-core/ && mvn ${MAVEN_OPTS} -Dtest=$TEST_LIST -DNPEFIX_VERSION=$NPEFIX_VERSION clean test -B -f $TEST_PATH
+mvn clean install -B -f src/repairnator-core/ -DskipTests && \
+mvn clean install -B -f src/repairnator-pipeline/ -DskipTests && \
+mvn ${MAVEN_OPTS} -Dtest=$TEST_LIST -DNPEFIX_VERSION=$NPEFIX_VERSION clean test -B -f $TEST_PATH
