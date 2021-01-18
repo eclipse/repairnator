625da046314b0ed1b998d095cd55118a5ed179a4
André Silva <andre15andre@hotmail.com>
2021-01-18 16:13:08 +0000

    Move Pair to repairnator-core
    Signed-off-by: André Silva <andre15andre@hotmail.com>

---
+++ src/repairnator-core/src/main/java/fr/inria/spirals/repairnator/utils/Pair.java
@@ -0,0 +1,22 @@
+package fr.inria.spirals.repairnator.utils;
+
+/**
+ * Created by thomas on 10/07/17.
+ */
+public class Pair<K,V> {
+	private K key;
+	private V value;
+
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+}
