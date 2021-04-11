f27d03996b446bce60cfaa1dfd46eb4c6ddadf5c
Khashayar <khaes@kth.se>
2021-04-11 18:39:59 +0200

    add commit-info initialization

--- src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/models/SoraldTargetCommit.java
+++ src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/models/SoraldTargetCommit.java
@@ -1,10 +1,21 @@
 package fr.inria.spirals.repairnator.process.step.repair.soraldbot.models;
 
 public class SoraldTargetCommit {
+    private static final String REPO_URL_TEMPLATE = "https://github.com/{slug}/";
+    private static final String COMMIT_URL_TEMPLATE = REPO_URL_TEMPLATE + "commit/{commit-id}";
+
     private String commitUrl;
     private String commitId;
     private String repoUrl;
     private String repoName;
+
+    public SoraldTargetCommit(String commitId, String repoName){
+        this.repoName = repoName;
+        this.commitId = commitId;
+
+        this.repoUrl = REPO_URL_TEMPLATE.replace("{slug}", repoName);
+        this.commitUrl = COMMIT_URL_TEMPLATE.replace("{slug}", repoName).replace("{commit-id}", commitId);
+    }
 
     public String getCommitUrl() {
         return commitUrl;
