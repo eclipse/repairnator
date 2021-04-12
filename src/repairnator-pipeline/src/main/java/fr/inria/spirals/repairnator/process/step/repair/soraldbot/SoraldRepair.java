7072eed8c7bad4f9dbe01e1a9ecf202e57aea176
Khashayar <khaes@kth.se>
2021-04-12 19:10:18 +0200

    cleaned up and moved to IN_PLACE mode

--- src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/SoraldRepair.java
+++ src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/SoraldRepair.java
@@ -5,21 +5,23 @@ import fr.inria.spirals.repairnator.process.step.StepStatus;
 import fr.inria.spirals.repairnator.process.step.repair.AbstractRepairStep;
 import fr.inria.spirals.repairnator.process.step.repair.soraldbot.models.SoraldTargetCommit;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
-
-import static fr.inria.spirals.repairnator.process.step.repair.soraldbot.SoraldConstants.*;
+import java.util.Set;
 
 public class SoraldRepair extends AbstractRepairStep {
+    private static final String REPO_PATH = "repo";
+    private static final String RULE_LINK_TEMPLATE = "https://rules.sonarsource.com/java/RSPEC-";
     private SoraldTargetCommit commit;
 
-    private void init(){
+    private void init() {
         commit = new SoraldTargetCommit(getInspector().getGitCommit(), getInspector().getRepoSlug());
     }
 
     @Override
     public String getRepairToolName() {
-        return SORALD_TOOL_NAME;
+        return SoraldConstants.SORALD_TOOL_NAME;
     }
 
     @Override
@@ -28,10 +30,14 @@ public class SoraldRepair extends AbstractRepairStep {
 
         List<String> rules = Arrays.asList(RepairnatorConfig.getInstance().getSonarRules());
         try {
-            SoraldCIAdapter.getInstance(getInspector().getWorkspace(), SPOON_SNIPER_MODE)
-                    .repairAndCreateForks(commit, rules);
+            for (String rule : rules) {
+                Set<String> patchedFiles =
+                        SoraldAdapter.getInstance(getInspector().getWorkspace(), SoraldConstants.SPOON_SNIPER_MODE)
+                                .repairRepoAndReturnViolationIntroducingFiles(commit, rule, REPO_PATH);
+
+            }
         } catch (Exception e) {
-            return StepStatus.buildSkipped(this,"Error while repairing with Sorald");
+            return StepStatus.buildSkipped(this, "Error while repairing with Sorald");
         }
 
         return StepStatus.buildSuccess(this);
