7662eaf22e212adad5cf6de10a91b0eed66ef9f9
Javier Ron <javierron90@gmail.com>
2021-06-04 13:11:39 +0200

    another flaky test
    Signed-off-by: Javier Ron <javierron90@gmail.com>

--- src/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/pipeline/TestPipeline.java
+++ src/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/pipeline/TestPipeline.java
@@ -91,8 +91,7 @@ public class TestPipeline {
         Launcher l = new Launcher(new String[]{
                 "--gitrepo",
                 "--gitrepourl", "https://github.com/repairnator/failingProject",
-                "--gitrepoidcommit", "883bc40f01902654b1b1df094b2badb28e192097",
-                "--gitrepobranch", "nofixes",
+                "--gitrepobranch", "no-infinite-loop",
                 "--launcherMode", "GIT_REPOSITORY",
                 "--workspace", workspaceFolder.getRoot().getAbsolutePath(),
                 "--output", outputFolder.getRoot().getAbsolutePath()
