648641428609ef2a3f1dd908d6f06e2a0c7859f5
Khashayar Etemadi <khaes@kth.se>
2021-06-09 10:19:33 +0200

    OriginalBranchName is retrieved automatically in SoraldBot

--- src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/SoraldBot.java
+++ src/repairnator-pipeline/src/main/java/fr/inria/spirals/repairnator/process/step/repair/soraldbot/SoraldBot.java
@@ -10,25 +10,42 @@ import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+/*
+    Performs Sorald repair on a given commit for a given sonar_rule.
+    Note: getConfig().push should be set to true to create a PR.
+ */
 public class SoraldBot extends AbstractRepairStep {
     private static final String REPO_PATH = "tmp_repo";
     private static final String RULE_LINK_TEMPLATE = "https://rules.sonarsource.com/java/RSPEC-";
     private SoraldTargetCommit commit;
+    private String originalBranchName;
 
     private String workingRepoPath;
 
-    private void init() {
+    /**
+     * {@return if the initialization is successful}
+     */
+    private boolean init() {
         commit = new SoraldTargetCommit(getConfig().getGitCommitHash(), getInspector().getRepoSlug());
         workingRepoPath = getInspector().getWorkspace() + File.separator + REPO_PATH;
+
+        try {
+            originalBranchName = getOriginalBranch();
+        } catch (IOException e) {
+            getLogger().error("IOException while looking for the original branch");
+        }
+
+        return commit != null && workingRepoPath != null && originalBranchName != null;
     }
 
     @Override
@@ -38,7 +55,10 @@ public class SoraldBot extends AbstractRepairStep {
 
     @Override
     protected StepStatus businessExecute() {
-        init();
+        boolean successfulInit = init();
+        if(!successfulInit){
+            return StepStatus.buildSkipped(this, "Error while repairing with Sorald");
+        }
 
         List<String> rules = Arrays.asList(RepairnatorConfig.getInstance().getSonarRules());
         try {
@@ -49,6 +69,7 @@ public class SoraldBot extends AbstractRepairStep {
 
                 if(patchedFiles != null && !patchedFiles.isEmpty()){
                     this.getInspector().getJobStatus().setHasBeenPatched(true);
+//                    getConfig().setPush(true);
                     createPRWithSpecificPatchedFiles(patchedFiles, rule);
                 }
             }
@@ -86,7 +107,23 @@ public class SoraldBot extends AbstractRepairStep {
 
         
         pushPatches(forkedGit, forkedRepo, newBranchName);
-        createPullRequest(getInspector().getGitRepositoryBranch(), newBranchName);
+        createPullRequest(originalBranchName, newBranchName);
+    }
+
+    private String getOriginalBranch() throws IOException {
+        GitHub github = new GitHubBuilder().withOAuthToken(RepairnatorConfig.getInstance().getGithubToken()).build();
+        GHRepository repo = github.getRepository(getInspector().getRepoSlug());
+        Map<String, GHBranch> branches = repo.getBranches();
+        Optional<String> branch = branches
+                .keySet().stream()
+                .filter(key -> branches.get(key).getSHA1().equals(commit.getCommitId()))
+                .findFirst();
+
+        if(!branch.isPresent()){
+            getLogger().error("The branch of the commit was not found");
+            return null;
+        }
+        return branch.get();
     }
 
     private void applyPatches4Sonar(Set<String> violationIntroducingFiles, String ruleNumber)
