e81656e1577b28e88b668144924f186a3a6c518b
Sofi1410 <sofy.bobadilla@gmail.com>
2022-09-02 17:21:40 +0200

    more details in the exception at PatchFilter.java

--- src/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/utils/PatchFilter.java
+++ src/repairnator-realtime/src/main/java/fr/inria/spirals/repairnator/realtime/utils/PatchFilter.java
@@ -256,7 +256,8 @@ public class PatchFilter {
             URL u = new URL(urlStr);
             data = IOUtils.toString(u,"UTF-8");
         }catch (Exception e){
-
+            System.out.println("message"+e.getMessage());
+            System.out.println("cause"+ e.getCause().toString());
             throw new RuntimeException("Error while reading file from URL:" + urlStr + " -- type: " + e.getClass().getName());
         }
         return data;
