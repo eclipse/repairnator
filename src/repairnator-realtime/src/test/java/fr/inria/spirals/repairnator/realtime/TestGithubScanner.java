3a762bcd07a17bec4c421e15e467649fe45c319b
Sofi1410 <sofy.bobadilla@gmail.com>
2022-12-15 18:54:36 +0100

    updating the commit sha to test fetch method on GitHubScanner

--- src/repairnator-realtime/src/test/java/fr/inria/spirals/repairnator/realtime/TestGithubScanner.java
+++ src/repairnator-realtime/src/test/java/fr/inria/spirals/repairnator/realtime/TestGithubScanner.java
@@ -1,7 +1,6 @@
 package fr.inria.spirals.repairnator.realtime;
 
 import fr.inria.spirals.repairnator.InputBuild;
-import fr.inria.spirals.repairnator.realtime.githubapi.commits.GithubAPICommitAdapter;
 import fr.inria.spirals.repairnator.realtime.githubapi.commits.models.SelectedCommit;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -13,7 +12,6 @@ import org.mockito.internal.util.reflection.Whitebox;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -39,50 +37,53 @@ public class TestGithubScanner {
 
     @Test
     public void testFetchingAll() throws Exception {
-
+        // repo https://github.com/castor-software/depclean/commits/master
         Set<String> repos = new HashSet<String>(FileUtils.readLines(new File(getClass()
                         .getResource("/GithubScannerTest_repos.txt").getFile()), "UTF-8"));
         GithubScanner scanner = new GithubScanner();
         scanner.setup();
 
         List<SelectedCommit> commits =
-                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020").getTime(),
-                        new SimpleDateFormat("dd/MM/yyyy").parse("27/01/2021").getTime(),
+                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("11/06/2022").getTime(),
+                        new SimpleDateFormat("dd/MM/yyyy").parse("14/12/2022").getTime(),
                         GithubScanner.FetchMode.ALL, repos);
-        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("290bdc01884f7c9bf2140a7c66d22ca72fe50fbb")));
-        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("e3456813fdcca1ed5075c0fb72b0bcbc9524e791")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("2693a1e2da8d74135c7c9be1e1aa5758c30f86f7")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("4460f47ef5f7c482efc9b953312e79ebfc0eebdd")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("36b10489c6ae71fba089c8db551d0ce2663d19d4")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("7411d15444a88cd5832364f3434ea66779de5918")));
+
     }
 
     @Test
     public void testFetchingFailed() throws Exception {
-
+        // repo https://github.com/castor-software/depclean/commits/master
         Set<String> repos = new HashSet<String>(FileUtils.readLines(new File(getClass()
                 .getResource("/GithubScannerTest_repos.txt").getFile()), "UTF-8"));
         GithubScanner scanner = new GithubScanner();
         scanner.setup();
 
         List<SelectedCommit> commits =
-                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020").getTime(),
-                        new SimpleDateFormat("dd/MM/yyyy").parse("27/01/2021").getTime(),
+                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("11/06/2022").getTime(),
+                        new SimpleDateFormat("dd/MM/yyyy").parse("14/12/2022").getTime(),
                         GithubScanner.FetchMode.FAILED, repos);
-        assertFalse(commits.stream().anyMatch(x -> x.getCommitId().equals("290bdc01884f7c9bf2140a7c66d22ca72fe50fbb")));
-        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("e3456813fdcca1ed5075c0fb72b0bcbc9524e791")));
+        assertFalse(commits.stream().anyMatch(x -> x.getCommitId().equals("2693a1e2da8d74135c7c9be1e1aa5758c30f86f7")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("4460f47ef5f7c482efc9b953312e79ebfc0eebdd")));
     }
 
     @Test
     public void testFetchingPassing() throws Exception {
-
+        // repo https://github.com/castor-software/depclean/commits/master
         Set<String> repos = new HashSet<String>(FileUtils.readLines(new File(getClass()
                 .getResource("/GithubScannerTest_repos.txt").getFile()), "UTF-8"));
         GithubScanner scanner = new GithubScanner();
         scanner.setup();
 
         List<SelectedCommit> commits =
-                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020").getTime(),
-                        new SimpleDateFormat("dd/MM/yyyy").parse("27/01/2021").getTime(),
+                scanner.fetch(new SimpleDateFormat("dd/MM/yyyy").parse("11/06/2022").getTime(),
+                        new SimpleDateFormat("dd/MM/yyyy").parse("14/12/2022").getTime(),
                         GithubScanner.FetchMode.PASSING, repos);
-        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("290bdc01884f7c9bf2140a7c66d22ca72fe50fbb")));
-        assertFalse(commits.stream().anyMatch(x -> x.getCommitId().equals("e3456813fdcca1ed5075c0fb72b0bcbc9524e791")));
+        assertTrue(commits.stream().anyMatch(x -> x.getCommitId().equals("36b10489c6ae71fba089c8db551d0ce2663d19d4")));
+        assertFalse(commits.stream().anyMatch(x -> x.getCommitId().equals("7411d15444a88cd5832364f3434ea66779de5918")));
     }
 
 }
