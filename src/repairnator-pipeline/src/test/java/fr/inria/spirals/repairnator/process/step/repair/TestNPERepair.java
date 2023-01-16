02eabcf005c8f2a796d35c1743c786103feec194
Sofi1410 <sofy.bobadilla@gmail.com>
2023-01-16 15:20:26 +0100

    @Ignore testNPERepair to debug

--- src/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/process/step/repair/TestNPERepair.java
+++ src/repairnator-pipeline/src/test/java/fr/inria/spirals/repairnator/process/step/repair/TestNPERepair.java
@@ -24,6 +24,7 @@ import org.hamcrest.core.Is;
 import org.hamcrest.core.IsNull;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -58,6 +59,7 @@ public class TestNPERepair {
     }
 
     @Test
+    @Ignore
     public void testNPERepair() throws IOException {
         long buildId = 220951790; // repairnator/failingProject simple-npe rerun on 23/01/13
         RepairnatorConfig.getInstance().setOutputPath(Files.createTempDirectory("test_nperepair_output").toFile().getAbsolutePath());
